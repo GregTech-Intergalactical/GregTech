41924c1ed11fb40137d583c53023245a32443271
qubka <ushakovn@tcd.ie>
2020-03-27 14:56:55 +0000

    Update TileBatteryBufferCreative.java

--- src/main/java/muramasa/gti/tile/single/TileBatteryBufferCreative.java
+++ src/main/java/muramasa/gti/tile/single/TileBatteryBufferCreative.java
@@ -1,6 +1,5 @@
 package muramasa.gti.tile.single;
 
-import muramasa.antimatter.Data;
 import muramasa.antimatter.capability.impl.MachineConfigHandler;
 import muramasa.antimatter.capability.impl.MachineEnergyHandler;
 import muramasa.antimatter.tile.TileEntityMachine;
@@ -9,40 +8,43 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.text.StringTextComponent;
+
 import java.util.Optional;
+import java.util.Random;
 
 public class TileBatteryBufferCreative extends TileEntityMachine {
-    private int amperage = 1;
+    private boolean storage;
 
     @Override
     public void onLoad() {
         super.onLoad();
+        storage = new Random().nextInt(1) == 0;
         configHandler = Optional.of(new BufferConfigHandler(this));
         energyHandler = Optional.of(new BufferEnergyHandler(this));
     }
+
     static class BufferConfigHandler extends MachineConfigHandler {
         public BufferConfigHandler(TileEntityMachine tile) {
             super(tile);
         }
         @Override
         public boolean onInteract(PlayerEntity player, Hand hand, Direction side, AntimatterToolType type) {
-            if (type == Data.SCREWDRIVER) {
-
+            if (!getTile().getWorld().isRemote && hand == Hand.MAIN_HAND) {
+                ((TileBatteryBufferCreative) getTile()).energyHandler.ifPresent(x -> {
+                    player.sendMessage(new StringTextComponent("Energy: " + x.getPower()));
+                });
             }
-            if (++((TileBatteryBufferCreative)getTile()).amperage > 16)
-                ((TileBatteryBufferCreative)getTile()).amperage = 1;
 
-            player.sendMessage(new StringTextComponent("Amperage: " + ((TileBatteryBufferCreative)getTile()).amperage));
-
-            return super.onInteract(player, hand, side, type);
+            return false;
         }
     }
-    static class BufferEnergyHandler extends  MachineEnergyHandler {
+
+    static class BufferEnergyHandler extends MachineEnergyHandler {
         TileBatteryBufferCreative tile;
         public BufferEnergyHandler(TileBatteryBufferCreative tile) {
             super(tile);
             this.tile = tile;
-            this.energy = Integer.MAX_VALUE;
+            this.energy = tile.storage ? Long.MAX_VALUE : 0L;
         }
     }
 }
