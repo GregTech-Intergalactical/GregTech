1cfe1df44571ebe74c7af6a218e85900f4ac2f09
qubka <ushakovn@tcd.ie>
2020-04-07 00:20:42 +0100

    Update for tesseract

--- src/main/java/muramasa/gti/block/BlockBatteryBufferCreative.java
+++ src/main/java/muramasa/gti/block/BlockBatteryBufferCreative.java
@@ -2,11 +2,12 @@ package muramasa.gti.block;
 
 import muramasa.antimatter.machine.Tier;
 import muramasa.antimatter.machine.types.Machine;
+import muramasa.antimatter.texture.Texture;
 import muramasa.gti.tile.single.TileBatteryBufferCreative;
 
 import static muramasa.antimatter.machine.MachineFlag.*;
 
-public class BlockBatteryBufferCreative extends Machine {
+public class BlockBatteryBufferCreative extends Machine<BlockBatteryBufferCreative> {
 
     public BlockBatteryBufferCreative(String domain, String id, Object... data) {
         super(domain, id, data);
--- src/main/java/muramasa/gti/tile/single/TileBatteryBufferCreative.java
+++ src/main/java/muramasa/gti/tile/single/TileBatteryBufferCreative.java
@@ -5,29 +5,31 @@ import muramasa.antimatter.capability.impl.MachineEnergyHandler;
 import muramasa.antimatter.machine.types.Machine;
 import muramasa.antimatter.tile.TileEntityMachine;
 import muramasa.antimatter.tool.AntimatterToolType;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Random;
 
 public class TileBatteryBufferCreative extends TileEntityMachine {
 
-    private boolean full;
-
-    public TileBatteryBufferCreative(Machine type) {
+    public TileBatteryBufferCreative(Machine<?> type) {
         super(type);
     }
 
     @Override
-    public void onLoad() {
-        if (!isServerSide()) return;
-        full = new Random().nextInt(2) == 0;
+    public void initCaps() {
         configHandler = Optional.of(new BufferConfigHandler(this));
         energyHandler = Optional.of(new BufferEnergyHandler(this));
-        super.onLoad();
+        super.initCaps();
     }
 
     static class BufferConfigHandler extends MachineConfigHandler {
@@ -39,7 +41,7 @@ public class TileBatteryBufferCreative extends TileEntityMachine {
 
         @Override
         public boolean onInteract(PlayerEntity player, Hand hand, Direction side, AntimatterToolType type) {
-            if (!tile.getWorld().isRemote && hand == Hand.MAIN_HAND) {
+            if (!Objects.requireNonNull(tile.getWorld()).isRemote && hand == Hand.MAIN_HAND) {
                 tile.energyHandler.ifPresent(h -> {
                     tile.getInfo().forEach(string -> player.sendMessage(new StringTextComponent(string)));
                 });
@@ -55,20 +57,56 @@ public class TileBatteryBufferCreative extends TileEntityMachine {
 
     static class BufferEnergyHandler extends MachineEnergyHandler {
         TileBatteryBufferCreative tile;
+
         public BufferEnergyHandler(TileBatteryBufferCreative tile) {
-            super(tile);
+            super(tile, true);
             this.tile = tile;
-            if (tile.full) {
-                this.energy = Integer.MAX_VALUE;
-                this.capacity = Integer.MAX_VALUE;
-                this.input = 0;
-                this.output = tile.getMachineTier().getVoltage();
-            } else {
-                this.energy = 0;
-                this.capacity = Integer.MAX_VALUE;
-                this.input = tile.getMachineTier().getVoltage();
-                this.output = 0;
+            boolean storage = new Random().nextInt(2) == 1;
+            this.energy = storage ? Integer.MAX_VALUE : 0;
+            this.capacity = Integer.MAX_VALUE;
+            this.voltage_in = tile.getMachineTier().getVoltage();
+            this.voltage_out = tile.getMachineTier().getVoltage();
+            this.amperage_in = storage ? 0 : 4;
+            this.amperage_out = storage ? 4 : 0;
+        }
+
+        @Override
+        public void onOverVoltage(long node) {
+            World world = tile.getWorld();
+            if (world != null) {
+                BlockPos pos = BlockPos.fromLong(node);
+                if (!world.isRemote)
+                    world.createExplosion(null, pos.getX(), pos.getY() + 0.0625D, pos.getZ(), 4.0F, Explosion.Mode.BREAK);
+                else
+                    world.addParticle(ParticleTypes.SMOKE, pos.getX(), pos.getY() + 0.5D, pos.getZ(), 0.0D, 0.0D, 0.0D);
+                world.setBlockState(pos, Blocks.AIR.getDefaultState());
             }
+        }
+
+        @Override
+        public void onOverAmperage(long cable) {
+            World world = tile.getWorld();
+            if (world != null) {
+                BlockPos pos = BlockPos.fromLong(cable); boolean fired = false;
+                for (Direction direction : Direction.values()) {
+                    BlockPos offset = pos.offset(direction);
+                    if (world.getBlockState(offset) == Blocks.AIR.getDefaultState()) {
+                        world.setBlockState(offset, Blocks.FIRE.getDefaultState());
+                        fired = true;
+                    }
+                }
+                if (!fired) world.setBlockState(pos, Blocks.AIR.getDefaultState());
+            }
+        }
+
+        /*@Override
+        public long insert(long toInsert, boolean simulate) {
+            return 0L;
+        }*/
+
+        @Override
+        public long extract(long toInsert, boolean simulate) {
+            return 0L;
         }
     }
 }
