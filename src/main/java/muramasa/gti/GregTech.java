8cc47959d7fe7e1b76f1752aba4b3a3b63fe9f2d
qubka <ushakovn@tcd.ie>
2020-04-19 09:38:59 +0100

    Remove unused imports

--- src/main/java/muramasa/gti/GregTech.java
+++ src/main/java/muramasa/gti/GregTech.java
@@ -12,7 +12,6 @@ import muramasa.gti.datagen.ProgressionAdvancements;
 import muramasa.gti.loader.WorldGenLoader;
 import muramasa.gti.proxy.ClientHandler;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
