4e08f10487a059a3fd54086519e3b6cce217680e
Albert Sjölund <albertsjolund@hotmail.com>
2020-06-10 11:50:13 +0200

    Update creative battery buffer

--- src/main/java/muramasa/gti/tile/single/TileBatteryBufferCreative.java
+++ src/main/java/muramasa/gti/tile/single/TileBatteryBufferCreative.java
@@ -19,19 +19,17 @@ public class TileBatteryBufferCreative extends TileEntityStorage {
 
     @Override
     public void onLoad() {
-        if (isServerSide() && has(ENERGY)) energyHandler = Optional.of(new MachineEnergyHandler(this, getMachineTier().getVoltage() * 64L, getMachineTier().getVoltage() * 64L, getMachineTier().getVoltage(), getMachineTier().getVoltage(), 1, 1) {
+        //Anonymous inherited classes are annoying since you have to rewrite code. probably move the energy handlers to an actual class.
+        if (/*isServerSide() &&*/ has(ENERGY)) energyHandler = Optional.of(new MachineEnergyHandler(this, getMachineTier().getVoltage() * 64L, getMachineTier().getVoltage() * 64L, getMachineTier().getVoltage(), getMachineTier().getVoltage(), 1, 1) {
             @Override
             public long extract(long maxExtract, boolean simulate) {
                 return maxExtract;
             }
 
-            @Override
-            public boolean canChargeItem() {
-                return true;
-            }
-
             public boolean canOutput(@Nonnull Dir direction) {
-                return tile.getOutputFacing().getIndex() == direction.getIndex();
+                //TODO: For debugging, set to always true for simplicity.
+                return true;
+               // return tile.getOutputFacing().getIndex() == direction.getIndex();
             }
         });
         super.onLoad();
