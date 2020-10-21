f82c333dae0200942b846111c49fbbb600c3f4ac
Abbe <albertsjolund@hotmail.com>
2020-10-22 01:53:03 +0200

    update conveyor and antimatter

--- src/main/java/muramasa/gti/cover/CoverConveyor.java
+++ src/main/java/muramasa/gti/cover/CoverConveyor.java
@@ -7,8 +7,15 @@ import muramasa.antimatter.machine.Tier;
 import muramasa.antimatter.tile.TileEntityMachine;
 import muramasa.antimatter.util.Utils;
 import muramasa.gti.Ref;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -54,9 +61,23 @@ public class CoverConveyor extends CoverTiered {
     public void onUpdate(CoverInstance instance, Direction side) {
         if (instance.getTile() == null || instance.getTile().getWorld().getGameTime() % (speeds.get(tier)) != 0)
             return;
+        boolean isMachine = instance.getTile() instanceof TileEntityMachine;
+        BlockState state = instance.getTile().getWorld().getBlockState(instance.getTile().getPos().offset(side));
+
+        //Drop into world.
+        if (state == Blocks.AIR.getDefaultState() && isMachine) {
+            World world = instance.getTile().getWorld();
+            BlockPos pos = instance.getTile().getPos();
+            ItemStack stack = ((TileEntityMachine)instance.getTile()).itemHandler.map(ih -> Utils.extractAny(ih.getOutputHandler())).orElse(ItemStack.EMPTY);
+            if (stack.isEmpty()) return;
+            world.addEntity(new ItemEntity(world,pos.getX()+side.getXOffset(), pos.getY()+side.getYOffset(), pos.getZ()+side.getZOffset(),stack));
+        }
+        if (!(state.hasTileEntity())) return;
         TileEntity adjTile = instance.getTile().getWorld().getTileEntity(instance.getTile().getPos().offset(side));
-        if (adjTile == null) return;
-        if (instance.getTile() instanceof TileEntityMachine) {
+        if (adjTile == null) {
+            return;
+        }
+        if (isMachine) {
             ((TileEntityMachine)instance.getTile()).itemHandler.ifPresent(ih -> adjTile.getCapability(ITEM_HANDLER_CAPABILITY).ifPresent(other -> Utils.transferItems(ih.getOutputHandler(), other)));
         } else {
             Utils.transferItemsOnCap(instance.getTile(), adjTile);
