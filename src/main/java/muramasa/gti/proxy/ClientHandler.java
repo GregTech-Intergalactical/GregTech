72f788b99e76fa520da8fd628b7042af03a1099c
Abbe <albertsjolund@hotmail.com>
2021-03-20 20:03:17 +0100

    Remove screensetup from GTI

--- src/main/java/muramasa/gti/proxy/ClientHandler.java
+++ src/main/java/muramasa/gti/proxy/ClientHandler.java
@@ -1,13 +1,6 @@
 package muramasa.gti.proxy;
 
 import muramasa.antimatter.AntimatterAPI;
-import muramasa.antimatter.Data;
-import muramasa.antimatter.client.ScreenSetup;
-import muramasa.antimatter.gui.container.ContainerCover;
-import muramasa.antimatter.gui.container.ContainerHatch;
-import muramasa.antimatter.gui.container.ContainerMachine;
-import muramasa.antimatter.gui.container.ContainerMultiMachine;
-import muramasa.antimatter.gui.screen.*;
 import muramasa.gti.block.BlockCasing;
 import muramasa.gti.data.GregTechData;
 import net.minecraft.client.renderer.RenderType;
@@ -23,11 +16,5 @@ public class ClientHandler {
             //AntimatterAPI.all(BlockCasingMachine.class, t -> RenderTypeLookup.setRenderLayer(t, RenderType.getCutout()));
             AntimatterAPI.all(BlockCasing.class, t -> RenderTypeLookup.setRenderLayer(t, RenderType.getCutout()));
         });
-
-        ScreenSetup.<ContainerMachine, ScreenBasicMachine<ContainerMachine>>setScreenMapping(Data.BASIC_MENU_HANDLER, ScreenBasicMachine::new);
-        ScreenSetup.<ContainerCover, ScreenCover<ContainerCover>>setScreenMapping(Data.COVER_MENU_HANDLER, ScreenCover::new);
-        ScreenSetup.<ContainerMultiMachine, ScreenMultiMachine<ContainerMultiMachine>>setScreenMapping(Data.MULTI_MENU_HANDLER, ScreenMultiMachine::new);
-        ScreenSetup.<ContainerHatch, ScreenHatch<ContainerHatch>>setScreenMapping(Data.HATCH_MENU_HANDLER, ScreenHatch::new);
-        ScreenSetup.<ContainerMachine, ScreenSteamMachine<ContainerMachine>>setScreenMapping(Data.STEAM_MENU_HANDLER, ScreenSteamMachine::new);
     }
 }
