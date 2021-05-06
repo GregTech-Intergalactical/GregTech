459be776990559d32d113e495b8a826a23394869
Abbe <albertsjolund@hotmail.com>
2021-05-06 21:28:12 +0200

    fix steam machine

--- src/main/java/muramasa/gti/tile/single/TileEntitySteamMachine.java
+++ src/main/java/muramasa/gti/tile/single/TileEntitySteamMachine.java
@@ -5,8 +5,11 @@ import muramasa.antimatter.machine.Tier;
 import muramasa.antimatter.machine.types.Machine;
 import muramasa.antimatter.recipe.Recipe;
 import muramasa.antimatter.tile.TileEntityMachine;
+import muramasa.antimatter.util.TagUtils;
 import muramasa.gti.data.Materials;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
 
 import java.util.Arrays;
 
@@ -31,6 +34,11 @@ public class TileEntitySteamMachine extends TileEntityMachine {
             @Override
             protected int getOverclock() {
                 return tile.getMachineTier() == BRONZE ? 0 : 1;
+            }
+
+            @Override
+            public boolean accepts(FluidStack stack) {
+                return stack.getFluid().getTags().contains(new ResourceLocation("forge", "steam"));
             }
         });
     }
