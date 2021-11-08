342a8216f4edf3404d185fb29770a2b1df6da7a9
Abbe <albertsjolund@hotmail.com>
2021-11-08 16:38:31 +0100

    Add steam machine event, update antimatter

--- src/main/java/muramasa/gti/tile/single/TileEntitySteamMachine.java
+++ src/main/java/muramasa/gti/tile/single/TileEntitySteamMachine.java
@@ -2,6 +2,8 @@ package muramasa.gti.tile.single;
 
 import muramasa.antimatter.capability.machine.MachineRecipeHandler;
 import muramasa.antimatter.machine.Tier;
+import muramasa.antimatter.machine.event.ContentEvent;
+import muramasa.antimatter.machine.event.IMachineEvent;
 import muramasa.antimatter.machine.types.Machine;
 import muramasa.antimatter.recipe.Recipe;
 import muramasa.antimatter.tile.TileEntityMachine;
@@ -33,6 +35,20 @@ public class TileEntitySteamMachine<T extends TileEntitySteamMachine<T>> extends
             protected boolean validateRecipe(Recipe r) {
                 List<ItemStack> consumed = this.tile.itemHandler.map(t -> t.consumeInputs(r, true)).orElse(Collections.emptyList());
                 return r.getPower() <= Tier.LV.getVoltage() && consumed.size() > 0;
+            }
+
+            @Override
+            public void onMachineEvent(IMachineEvent event, Object... data) {
+                super.onMachineEvent(event, data);
+                if (event instanceof ContentEvent) {
+                    if (event == ContentEvent.FLUID_INPUT_CHANGED) {
+                        if (data != null && data.length > 0) {
+                            if (data[0] instanceof FluidStack && ((FluidStack)data[0]).getFluid().getTags().contains(STEAM)) {
+                                checkRecipe();
+                            }
+                        }
+                    }
+                }
             }
 
             @Override
