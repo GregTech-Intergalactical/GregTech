cf112178158ca12c57d586df13545e6f44d2af10
Abbe <xvlirox@gmail.com>
2022-03-01 15:27:16 +0100

    forgot large hull

--- src/main/java/muramasa/gregtech/loader/machines/AssemblyLoader.java
+++ src/main/java/muramasa/gregtech/loader/machines/AssemblyLoader.java
@@ -61,7 +61,7 @@ public class AssemblyLoader {
     private static void batteries() {
         ASSEMBLING.RB().ii(PLATE.getIngredient(BatteryAlloy,1), RecipeIngredient.of(TIER_CABLES.get(Tier.LV) ,1)).fi(Polyethylene.getLiquid(144)).io(BatteryHullSmall.getDefaultInstance()).add(80, 2);
         ASSEMBLING.RB().ii(PLATE.getIngredient(BatteryAlloy,3), RecipeIngredient.of(TIER_CABLES.get(Tier.MV) ,2)).fi(Polyethylene.getLiquid(432)).io(BatteryHullMedium.getDefaultInstance()).add(120, 4);
-        ASSEMBLING.RB().ii(PLATE.getIngredient(BatteryAlloy,9), RecipeIngredient.of(TIER_CABLES.get(Tier.HV) ,3)).fi(Polyethylene.getLiquid(1296)).io(BatteryHullMedium.getDefaultInstance()).add(160, 8);
+        ASSEMBLING.RB().ii(PLATE.getIngredient(BatteryAlloy,9), RecipeIngredient.of(TIER_CABLES.get(Tier.HV) ,3)).fi(Polyethylene.getLiquid(1296)).io(BatteryHullLarge.getDefaultInstance()).add(160, 8);
     }
 
     private static void casings() {
