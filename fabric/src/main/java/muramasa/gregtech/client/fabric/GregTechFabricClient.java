eb13fe5880df4aaa5d33ea8e3e74de263353fd71
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-09-26 16:39:28 -0400

    changed to antimatter client initializer

--- fabric/src/main/java/muramasa/gregtech/client/fabric/GregTechFabricClient.java
+++ fabric/src/main/java/muramasa/gregtech/client/fabric/GregTechFabricClient.java
@@ -1,9 +1,10 @@
 package muramasa.gregtech.client.fabric;
 
+import muramasa.antimatter.client.fabric.IAntimatterClientInitializer;
 import muramasa.gregtech.proxy.ClientHandler;
 import net.fabricmc.api.ClientModInitializer;
 
-public class GregTechFabricClient implements ClientModInitializer {
+public class GregTechFabricClient implements IAntimatterClientInitializer {
     @Override
     public void onInitializeClient() {
         ClientHandler.setup();
