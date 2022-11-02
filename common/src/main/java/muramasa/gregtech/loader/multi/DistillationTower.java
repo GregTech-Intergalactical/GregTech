fd3b4977b0802669cb8ec1b7ec6bf39d86602076
Matthew Bentham <lynxx131@outlook.com>
2022-11-02 14:05:41 +1000

    Add distillation tower recipes

--- common/src/main/java/muramasa/gregtech/loader/multi/DistillationTower.java
+++ common/src/main/java/muramasa/gregtech/loader/multi/DistillationTower.java
@@ -1,17 +1,22 @@
 package muramasa.gregtech.loader.multi;
 
-import static muramasa.gregtech.data.Materials.*;
+import muramasa.antimatter.recipe.map.RecipeBuilder;
+
+import static muramasa.antimatter.material.MaterialTags.DISTILLATION_FLUID_INPUT_AMOUNT;
+import static muramasa.antimatter.material.MaterialTags.DISTILL_INTO;
 import static muramasa.gregtech.data.RecipeMaps.DISTILLATION;
 
 public class DistillationTower {
     public static void init() {
-        DISTILLATION.RB().fi(Oil.getLiquid(100)).fo(SulfuricLightFuel.getLiquid(100), SulfuricHeavyFuel.getLiquid(30), SulfuricNaphtha.getLiquid(40), SulfuricGas.getGas(120),
-                SulfuricAcid.getLiquid(20)).add(15, 384);
-        DISTILLATION.RB().fi(OilMedium.getLiquid(100)).fo(SulfuricLightFuel.getLiquid(100), SulfuricHeavyFuel.getLiquid(30), SulfuricNaphtha.getLiquid(40), SulfuricGas.getGas(120),
-                SulfuricAcid.getLiquid(20)).add(15, 384);
-        DISTILLATION.RB().fi(OilHeavy.getLiquid(100)).fo(SulfuricLightFuel.getLiquid(20), SulfuricHeavyFuel.getLiquid(200), SulfuricNaphtha.getLiquid(10), SulfuricGas.getGas(100),
-                SulfuricAcid.getLiquid(30)).add(20, 384);
-        DISTILLATION.RB().fi(OilLight.getLiquid(125)).fo(SulfuricLightFuel.getLiquid(150), SulfuricHeavyFuel.getLiquid(10), SulfuricNaphtha.getLiquid(30), SulfuricGas.getGas(80),
-                SulfuricAcid.getLiquid(5)).add(10, 384);
+        DISTILL_INTO.getAll().forEach((material, fluidProducts) -> {
+           RecipeBuilder builder = DISTILLATION.RB()
+                   .fi(material.getLiquid(DISTILLATION_FLUID_INPUT_AMOUNT.getInt(material)));
+
+           for(int i=0;i<fluidProducts.size();i++){
+               builder.fo(fluidProducts.get(i).mat().getLiquid(fluidProducts.get(i).amount()));
+           }
+
+           builder.add(material.getMass(), 20);
+        });
     }
 }
