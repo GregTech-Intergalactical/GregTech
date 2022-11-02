eb65dc64dbb9046a2f46635a34064fcc7313ad71
Matthew Bentham <lynxx131@outlook.com>
2022-11-02 13:43:17 +1000

    Fix null reference exceptions

--- common/src/main/java/muramasa/gregtech/loader/machines/ExtruderLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/ExtruderLoader.java
@@ -19,43 +19,45 @@ public class ExtruderLoader {
     public static void init() {
         RING.all().forEach(r -> {
             if (!r.has(INGOT)) return;
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(r),1),of(GregTechData.ShapeRing,1).setNoConsume()).io(RING.get(r,4)).add(r.getElement().getHardness(),30);
+            long duration = r.getElement() == null ? Math.max(r.getMass(), 1) : r.getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(r),1),of(GregTechData.ShapeRing,1).setNoConsume()).io(RING.get(r,4)).add(duration,30);
         });
 
         GEAR.all().forEach(g -> {
             if (!g.has(INGOT)) return;
-            long duration = Math.max(g.getMass(), 1);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(g),4),of(GregTechData.ShapeGear,1).setNoConsume()).io(GEAR.get(g,1)).add(g.getElement().getHardness()*4,30);
+            long duration = g.getElement() == null ? Math.max(g.getMass(), 1) : g.getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(g),4),of(GregTechData.ShapeGear,1).setNoConsume()).io(GEAR.get(g,1)).add(duration*4,30);
         });
 
         BOLT.all().forEach(b -> {
             if (!b.has(INGOT)) return;
-            long duration = Math.max(b.getMass(), 1);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(b),1),of(GregTechData.ShapeBolt,1).setNoConsume()).io(BOLT.get(b,8)).add(b.getElement().getHardness(),30);
+            long duration = b.getElement() == null ? Math.max(b.getMass(), 1) : b.getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(b),1),of(GregTechData.ShapeBolt,1).setNoConsume()).io(BOLT.get(b,8)).add(duration,30);
         });
 
         GEAR_SMALL.all().forEach(g -> {
             if (!g.has(INGOT)) return;
-            long duration = Math.max(g.getMass(), 1);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(g),1),of(GregTechData.ShapeGearSmall,1).setNoConsume()).io(GEAR_SMALL.get(g,1)).add(g.getElement().getHardness(),30);
+            long duration = g.getElement() == null ? Math.max(g.getMass(), 1) : g.getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(g),1),of(GregTechData.ShapeGearSmall,1).setNoConsume()).io(GEAR_SMALL.get(g,1)).add(duration,30);
         });
 
         PLATE.all().forEach(p -> {
             if (!p.has(INGOT)) return;
-            long duration = Math.max(p.getMass(), 1);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(p),1),of(GregTechData.ShapePlate,1).setNoConsume()).io(PLATE.get(p,1)).add(p.getElement().getHardness(),30);
+            long duration = p.getElement() == null ? Math.max(p.getMass(), 1) : p.getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(p),1),of(GregTechData.ShapePlate,1).setNoConsume()).io(PLATE.get(p,1)).add(duration,30);
         });
 
         ROD.all().forEach(r -> {
             if (!r.has(INGOT)) return;
-            long duration = Math.max(r.getMass(), 1);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(r),1),of(GregTechData.ShapeRod,1).setNoConsume()).io(ROD.get(r,2)).add(r.getElement().getHardness(),30);
+            long duration = r.getElement() == null ? Math.max(r.getMass(), 1) : r.getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(r),1),of(GregTechData.ShapeRod,1).setNoConsume()).io(ROD.get(r,2)).add(duration,30);
         });
 
         AntimatterAPI.all(Wire.class).forEach(t -> {
             Item wireItem = t.getBlockItem(PipeSize.VTINY);
             ItemStack stack = new ItemStack(wireItem,2);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),1),of(GregTechData.ShapeWire,1).setNoConsume()).io(stack).add(t.getMaterial().getElement().getHardness(),30);
+            long duration = t.getMaterial().getElement() == null ? Math.max(t.getMaterial().getMass(), 1) : t.getMaterial().getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),1),of(GregTechData.ShapeWire,1).setNoConsume()).io(stack).add(duration,30);
         });
 
         AntimatterAPI.all(FluidPipe.class).forEach(t -> {
@@ -69,11 +71,12 @@ public class ExtruderLoader {
             ItemStack stackM = new ItemStack(pipeM,1);
             ItemStack stackL = new ItemStack(pipeL,2);
             ItemStack stackH = new ItemStack(pipeH,1);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeTiny,1).setNoConsume()).io(stackT).add(t.getMaterial().getElement().getHardness()*3,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeSmall,1).setNoConsume()).io(stackS).add(t.getMaterial().getElement().getHardness()*3,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeNormal,1).setNoConsume()).io(stackM).add(t.getMaterial().getElement().getHardness()*3,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeLarge,1).setNoConsume()).io(stackL).add(t.getMaterial().getElement().getHardness()*6,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeHuge,1).setNoConsume()).io(stackH).add(t.getMaterial().getElement().getHardness()*6,30);
+            long duration = t.getMaterial().getElement() == null ? Math.max(t.getMaterial().getMass(), 1) : t.getMaterial().getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeTiny,1).setNoConsume()).io(stackT).add(duration*3,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeSmall,1).setNoConsume()).io(stackS).add(duration*3,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeNormal,1).setNoConsume()).io(stackM).add(duration*3,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeLarge,1).setNoConsume()).io(stackL).add(duration*6,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeHuge,1).setNoConsume()).io(stackH).add(duration*6,30);
         });
 
         AntimatterAPI.all(ItemPipe.class).forEach(t -> {
@@ -87,11 +90,12 @@ public class ExtruderLoader {
             ItemStack stackM = new ItemStack(pipeM,1);
             ItemStack stackL = new ItemStack(pipeL,2);
             ItemStack stackH = new ItemStack(pipeH,1);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeTiny,1).setNoConsume()).io(stackT).add(t.getMaterial().getElement().getHardness()*3,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeSmall,1).setNoConsume()).io(stackS).add(t.getMaterial().getElement().getHardness()*3,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeNormal,1).setNoConsume()).io(stackM).add(t.getMaterial().getElement().getHardness()*3,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeLarge,1).setNoConsume()).io(stackL).add(t.getMaterial().getElement().getHardness()*6,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeHuge,1).setNoConsume()).io(stackH).add(t.getMaterial().getElement().getHardness()*6,30);
+            long duration = t.getMaterial().getElement() == null ? Math.max(t.getMaterial().getMass(), 1) : t.getMaterial().getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeTiny,1).setNoConsume()).io(stackT).add(duration*3,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeSmall,1).setNoConsume()).io(stackS).add(duration*3,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeNormal,1).setNoConsume()).io(stackM).add(duration*3,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeLarge,1).setNoConsume()).io(stackL).add(duration*6,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeHuge,1).setNoConsume()).io(stackH).add(duration*6,30);
         });
 
         AntimatterAPI.all(HeatPipe.class).forEach(t -> {
@@ -105,11 +109,12 @@ public class ExtruderLoader {
             ItemStack stackM = new ItemStack(pipeM,1);
             ItemStack stackL = new ItemStack(pipeL,2);
             ItemStack stackH = new ItemStack(pipeH,1);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeTiny,1).setNoConsume()).io(stackT).add(t.getMaterial().getElement().getHardness()*10,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeSmall,1).setNoConsume()).io(stackS).add(t.getMaterial().getElement().getHardness()*20,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeNormal,1).setNoConsume()).io(stackM).add(t.getMaterial().getElement().getHardness()*30,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeLarge,1).setNoConsume()).io(stackL).add(t.getMaterial().getElement().getHardness()*40,30);
-            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeHuge,1).setNoConsume()).io(stackH).add(t.getMaterial().getElement().getHardness()*50,30);
+            long duration = t.getMaterial().getElement() == null ? Math.max(t.getMaterial().getMass(), 1) : t.getMaterial().getElement().getHardness();
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeTiny,1).setNoConsume()).io(stackT).add(duration*10,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeSmall,1).setNoConsume()).io(stackS).add(duration*20,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),3),of(GregTechData.ShapePipeNormal,1).setNoConsume()).io(stackM).add(duration*30,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeLarge,1).setNoConsume()).io(stackL).add(duration*40,30);
+            EXTRUDING.RB().ii(of(INGOT.getMaterialTag(t.getMaterial()),6),of(GregTechData.ShapePipeHuge,1).setNoConsume()).io(stackH).add(duration*50,30);
         });
     }
 }
