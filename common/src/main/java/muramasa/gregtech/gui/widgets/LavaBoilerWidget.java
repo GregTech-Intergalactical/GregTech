4698533f84d76b3b96934da6a709ebe3da535a19
Matthew Bentham <lynxx131@outlook.com>
2022-11-23 13:53:58 +1000

    Implement Lava boiler

---
+++ common/src/main/java/muramasa/gregtech/gui/widgets/LavaBoilerWidget.java
@@ -0,0 +1,110 @@
+package muramasa.gregtech.gui.widgets;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import muramasa.antimatter.gui.GuiInstance;
+import muramasa.antimatter.gui.IGuiElement;
+import muramasa.antimatter.gui.Widget;
+import muramasa.antimatter.gui.container.ContainerMachine;
+import muramasa.antimatter.gui.widget.WidgetSupplier;
+import muramasa.antimatter.mixin.client.AbstractContainerScreenAccessor;
+import muramasa.gregtech.tile.single.TileEntityLavaBoiler;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.network.chat.TextComponent;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static muramasa.antimatter.gui.ICanSyncData.SyncDirection.SERVER_TO_CLIENT;
+
+public class LavaBoilerWidget extends Widget {
+    private int heat = 0, maxHeat = 0, water = 0, steam = 0;
+
+    protected LavaBoilerWidget(@Nonnull GuiInstance gui, @Nullable IGuiElement parent) {
+        super(gui, parent);
+    }
+
+    public static WidgetSupplier build() {
+        return builder(LavaBoilerWidget::new);
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        gui.syncInt(() -> ((TileEntityLavaBoiler)((ContainerMachine<?>)gui.container).getTile()).getHeat(), i -> heat = i, SERVER_TO_CLIENT);
+        gui.syncInt(() -> ((TileEntityLavaBoiler)((ContainerMachine<?>)gui.container).getTile()).getMaxHeat(), i -> maxHeat = i, SERVER_TO_CLIENT);
+        gui.syncInt(() -> ((ContainerMachine<?>)gui.container).getTile().fluidHandler.map(t -> t.getInputs()[0].getAmount()).orElse(0), i -> water = i, SERVER_TO_CLIENT);
+        gui.syncInt(() -> ((ContainerMachine<?>)gui.container).getTile().fluidHandler.map(t -> t.getOutputs()[0].getAmount()).orElse(0), i -> steam = i, SERVER_TO_CLIENT);
+    }
+
+    @Override
+    public void render(PoseStack stack, double mouseX, double mouseY, float partialTicks) {
+        if (water >= 1) {
+            float per = (float) water / 16000;
+            if (per > 1.0F) {
+                per = 1.0F;
+            }
+            int lvl = (int) (per * (float) 54);
+            if (lvl < 0) {
+                return;
+            }
+            int y = (realY() + 54) - lvl;
+            drawTexture(stack, gui.handler.getGuiTexture(), realX() + 13, y, ((AbstractContainerScreenAccessor)gui.screen).getImageWidth() + 28, 54 - lvl, 10, lvl);
+
+        }
+        if (steam >= 1) {
+            float per = (float) steam / 16000;
+            if (per > 1.0F) {
+                per = 1.0F;
+            }
+            int lvl = (int) (per * (float) 54);
+            if (lvl < 0) {
+                return;
+            }
+            int y = (realY() + 54) - lvl;
+            drawTexture(stack, gui.handler.getGuiTexture(), realX(), y, ((AbstractContainerScreenAccessor)gui.screen).getImageWidth() + 18, 54 - lvl, 10, lvl);
+        }
+        if (heat >= 1) {
+            float per = (float) heat / maxHeat;
+            if (per > 1.0F) {
+                per = 1.0F;
+            }
+            int lvl = (int) (per * (float) 54);
+            if (lvl < 0) {
+                return;
+            }
+            int y = (((AbstractContainerScreenAccessor)gui.screen).getTopPos() + 25 + 54) - lvl;
+            drawTexture(stack, gui.handler.getGuiTexture(), realX() + 26, y, ((AbstractContainerScreenAccessor)gui.screen).getImageWidth() + 38, 54 - lvl, 10, lvl);
+        }
+    }
+
+    @Override
+    public void mouseOver(PoseStack stack, double mouseX, double mouseY, float partialTicks) {
+        if (water >= 1) {
+            renderTooltip(stack,"Water: " + water + " MB", mouseX, mouseY, 14, 0, 10, 54);
+        }
+        if (steam >= 1) {
+            renderTooltip(stack,"Steam: " + steam + " MB", mouseX, mouseY, 0, 0, 10, 54);
+        }
+        renderTooltip(stack,"Heat: " + heat + "K out of " + maxHeat, mouseX, mouseY, 26, 0, 10, 54);
+    }
+
+    @Environment(EnvType.CLIENT)
+    protected void renderTooltip(PoseStack matrixStack, String text, double mouseX, double mouseY, int x, int y, int w, int h) {
+        if (isInside(x, y, w, h, mouseX, mouseY)){
+            renderTooltip(matrixStack, new TextComponent(text), mouseX, mouseY);
+        }
+
+    }
+
+    public boolean isInside(int x, int y, int w, int h, double mouseX, double mouseY) {
+        int realX = realX() + x;
+        int realY = realY() + y;
+        return ((mouseX >= realX && mouseX <= realX + w) && (mouseY >= realY && mouseY <= realY + h));
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        return false;
+    }
+}
---
+++ common/src/main/java/muramasa/gregtech/tile/single/TileEntityLavaBoiler.java
@@ -0,0 +1,292 @@
+package muramasa.gregtech.tile.single;
+
+import muramasa.antimatter.capability.fluid.FluidTanks;
+import muramasa.antimatter.capability.machine.MachineFluidHandler;
+import muramasa.antimatter.capability.machine.MachineRecipeHandler;
+import muramasa.antimatter.machine.MachineState;
+import muramasa.antimatter.machine.event.ContentEvent;
+import muramasa.antimatter.machine.types.Machine;
+import muramasa.antimatter.tile.TileEntityMachine;
+import muramasa.antimatter.util.Utils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static muramasa.antimatter.machine.MachineState.ACTIVE;
+import static muramasa.antimatter.machine.MachineState.IDLE;
+import static muramasa.antimatter.machine.Tier.BRONZE;
+import static muramasa.gregtech.data.Materials.DistilledWater;
+import static muramasa.gregtech.data.Materials.Steam;
+import static net.minecraftforge.fluids.capability.IFluidHandler.FluidAction.EXECUTE;
+
+public class TileEntityLavaBoiler extends TileEntityMachine<TileEntityLavaBoiler> {
+    int maxHeat = 500, heat, fuel = 0, maxFuel, lossTimer = 0;
+    boolean hadNoWater;
+
+    public TileEntityLavaBoiler(Machine<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+        fluidHandler.set(() -> new LavaBoilerFluidHandler(this));
+        recipeHandler.set(() -> new LavaBoilerRecipeHandler(this));
+    }
+
+    public int getFuel() {
+        return recipeHandler.map(r -> (((LavaBoilerRecipeHandler) r).getFuel())).orElse(0);
+    }
+
+    public int getHeat() {
+        return recipeHandler.map(r -> (((LavaBoilerRecipeHandler) r).getHeat())).orElse(0);
+    }
+
+    public int getMaxFuel() {
+        return recipeHandler.map(r -> (((LavaBoilerRecipeHandler) r).getMaxFuel())).orElse(0);
+
+    }
+
+    public int getMaxHeat() {
+        return recipeHandler.map(r -> (((LavaBoilerRecipeHandler) r).getMaxHeat())).orElse(0);
+    }
+
+    public static class LavaBoilerRecipeHandler extends MachineRecipeHandler<TileEntityLavaBoiler> {
+        int maxHeat = 500, heat, fuel = 0, maxFuel, lossTimer = 0;
+        boolean hadNoWater;
+
+        protected final ContainerData GUI_SYNC_DATA2 = new ContainerData() {
+
+            @Override
+            public int get(int index) {
+                switch (index) {
+                    case 0:
+                        return LavaBoilerRecipeHandler.this.heat;
+                    case 1:
+                        return LavaBoilerRecipeHandler.this.maxHeat;
+                    case 2:
+                        return LavaBoilerRecipeHandler.this.fuel;
+                    case 3:
+                        return LavaBoilerRecipeHandler.this.maxFuel;
+                }
+                return 0;
+            }
+
+            @Override
+            public void set(int index, int value) {
+                switch (index) {
+                    case 0:
+                        LavaBoilerRecipeHandler.this.heat = value;
+                        break;
+                    case 1:
+                        LavaBoilerRecipeHandler.this.maxHeat = value;
+                        break;
+                    case 2:
+                        LavaBoilerRecipeHandler.this.fuel = value;
+                        break;
+                    case 3:
+                        LavaBoilerRecipeHandler.this.maxFuel = value;
+                        break;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+        };
+
+        public LavaBoilerRecipeHandler(TileEntityLavaBoiler tile) {
+            super(tile);
+            GUI_SYNC_DATA2.set(0, 0);
+            maxHeat = tile.getMachineTier() == BRONZE ? 500 : 1000;
+        }
+
+        public int getFuel() {
+            return fuel;
+        }
+
+        public int getHeat() {
+            return heat;
+        }
+
+        public int getMaxFuel() {
+            return maxFuel;
+        }
+
+        public int getMaxHeat() {
+            return maxHeat;
+        }
+
+        @Override
+        public void onServerUpdate() {
+            if (this.heat <= 20) {
+                this.heat = 20;
+                this.lossTimer = 0;
+            }
+            int delay = tile.getMachineTier() == BRONZE ? 45 : 40;
+            if (++this.lossTimer > delay) {
+                this.heat -= 1;
+                this.lossTimer = 0;
+            }
+            Arrays.stream(Direction.values()).filter(f -> f != Direction.DOWN).collect(Collectors.toList()).forEach(this::exportFluidFromMachineToSide);
+            delay = tile.getMachineTier() == BRONZE ? 25 : 10;
+            if (tile.getLevel().getGameTime() % delay == 0) {
+                tile.fluidHandler.ifPresent(f -> {
+                    FluidStack[] inputs = f.getInputs();
+                    if (this.heat > 100) {
+                        if (inputs[0].getAmount() == 0) {
+                            hadNoWater = true;
+                        } else {
+                            if (hadNoWater) {
+                                tile.getLevel().explode(null, tile.getBlockPos().getX(), tile.getBlockPos().getY(), tile.getBlockPos().getZ(), 4.0F, Explosion.BlockInteraction.DESTROY);
+                                tile.getLevel().setBlockAndUpdate(tile.getBlockPos(), Blocks.AIR.defaultBlockState());
+                                return;
+                            }
+                            f.drainInput(new FluidStack(Fluids.WATER, 1), IFluidHandler.FluidAction.EXECUTE);
+                            int room = 16000 - f.getOutputs()[0].getAmount();
+                            int fill = Math.min(room, 150);
+                            if (room > 0){
+                                f.fillOutput(Steam.getGas(fill), IFluidHandler.FluidAction.EXECUTE);
+                            }
+                            if (fill < 150) {
+                                //TODO:steam sounds
+                                tile.getLevel().playSound(null, tile.getBlockPos(), SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 1.0f, 1.0f);
+                                if (tile.getLevel() instanceof ServerLevel)
+                                    ((ServerLevel) tile.getLevel()).sendParticles(ParticleTypes.SMOKE, tile.getBlockPos().getX(), tile.getBlockPos().getY(), tile.getBlockPos().getZ(), tile.getLevel().getRandom().nextInt(8) + 1, 0.0D, 0.2D, 0.0D, 0.0D);
+                                f.drain(4000, IFluidHandler.FluidAction.EXECUTE);
+                            }
+                        }
+                    } else {
+                        this.hadNoWater = false;
+                    }
+                });
+            }
+            super.onServerUpdate();
+        }
+
+        public void exportFluidFromMachineToSide(Direction side){
+            BlockEntity adjTile = tile.getLevel().getBlockEntity(tile.getBlockPos().relative(side));
+            if (adjTile == null) return;
+            LazyOptional<IFluidHandler> cap = adjTile.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, side.getOpposite());
+            tile.fluidHandler.ifPresent(f -> cap.ifPresent(other -> Utils.transferFluids(f.getOutputTanks(), other, 1000)));
+        }
+
+        @Override
+        protected MachineState recipeFinish() {
+            if (!canRecipeContinue()) {
+                this.resetRecipe();
+                return IDLE;
+            } else {
+                return ACTIVE;
+            }
+        }
+
+        @Override
+        protected MachineState tickRecipe() {
+            if (this.activeRecipe == null) {
+                System.out.println("Check Recipe when active recipe is null");
+                return tile.getMachineState();
+            }
+            else {
+                tile.onRecipePreTick();
+                if (fuel <= 0 && canRecipeContinue()) {
+                    if (fuel < 0) {
+                        fuel = 0;
+                    }
+                    this.maxFuel = activeRecipe.getDuration();
+                    addOutputs();
+                    this.fuel += maxFuel;
+                    consumeInputs();
+                }
+                if ((this.heat < maxHeat) && (this.fuel > 0) && (tile.getLevel().getGameTime() % 12L == 0L)) {
+                    int fuelSubtract = tile.getMachineTier() == BRONZE ? 1 : 2;
+                    this.fuel -= fuelSubtract;
+                    this.heat += 1;
+                }
+                tile.onRecipePostTick();
+                if (fuel == 0){
+                    return this.recipeFinish();
+                }
+                return ACTIVE;
+            }
+        }
+
+        public void setActive(boolean t){
+            if (t && tile.getMachineState() != ACTIVE){
+                tile.setMachineState(ACTIVE);
+            } else if (!t && tile.getMachineState() == ACTIVE){
+                tile.setMachineState(IDLE);
+            }
+        }
+
+        @Override
+        public boolean accepts(FluidStack fluid) {
+            return fluid.getFluid() == Fluids.WATER || fluid.getFluid() == DistilledWater.getLiquid();
+        }
+
+        @Override
+        public boolean canOutput() {
+            return true;
+        }
+
+        @Override
+        public CompoundTag serializeNBT() {
+            CompoundTag nbt = super.serializeNBT();
+            nbt.putInt("heat", heat);
+            nbt.putInt("maxHeat", maxHeat);
+            nbt.putInt("fuel", fuel);
+            nbt.putInt("maxFuel", maxFuel);
+            nbt.putInt("lossTimer", lossTimer);
+            nbt.putBoolean("hadNoWater", hadNoWater);
+            return nbt;
+        }
+
+        @Override
+        public void deserializeNBT(CompoundTag nbt) {
+            super.deserializeNBT(nbt);
+            this.heat = nbt.getInt("heat");
+            this.maxHeat = nbt.getInt("maxHeat");
+            this.fuel = nbt.getInt("fuel");
+            this.maxFuel = nbt.getInt("maxFuel");
+            this.lossTimer = nbt.getInt("lossTimer");
+            this.hadNoWater = nbt.getBoolean("hadNoWater");
+        }
+    }
+
+    public static class LavaBoilerFluidHandler extends MachineFluidHandler<TileEntityLavaBoiler> {
+
+        private boolean fillingCell = false;
+
+        public LavaBoilerFluidHandler(TileEntityLavaBoiler tile) {
+            super(tile, 16000, 1000 * (250 + tile.getMachineTier().getIntegerId()));
+            tanks.put(FluidDirection.INPUT, FluidTanks.create(tile, ContentEvent.FLUID_INPUT_CHANGED, b -> {
+                b.tank(16000);
+                return b;
+            }));
+            tanks.put(FluidDirection.OUTPUT, FluidTanks.create(tile, ContentEvent.FLUID_OUTPUT_CHANGED, b -> {
+                b.tank(16000);
+                return b;
+            }));
+        }
+
+        @Override
+        protected FluidTanks getCellAccessibleTanks() {
+            return getInputTanks();
+        }
+    }
+}
