15f42a0c7472b1094f4c61bc34c9c05aed3a4680
Rundas <38040866+Rundas01@users.noreply.github.com>
2022-12-17 09:39:53 +0100

    small gui changes to coke oven and bronze blast furnace

--- common/src/main/java/muramasa/gregtech/loader/multi/Coking.java
+++ common/src/main/java/muramasa/gregtech/loader/multi/Coking.java
@@ -2,13 +2,17 @@ package muramasa.gregtech.loader.multi;
 
 import muramasa.antimatter.recipe.ingredient.RecipeIngredient;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.level.block.Blocks;
 
+import static muramasa.antimatter.data.AntimatterMaterialTypes.BLOCK;
 import static muramasa.antimatter.data.AntimatterMaterialTypes.GEM;
-import static muramasa.gregtech.data.Materials.Charcoal;
+import static muramasa.gregtech.data.Materials.*;
 import static muramasa.gregtech.data.RecipeMaps.COKING;
 
 public class Coking {
     public static void init() {
-        COKING.RB().ii(RecipeIngredient.of(ItemTags.LOGS, 1)).io(GEM.get(Charcoal, 1)).add(600, 0);
+        COKING.RB().ii(RecipeIngredient.of(ItemTags.LOGS, 1)).io(GEM.get(Charcoal, 1)).fo(Creosote.getLiquid(100)).add(600, 0);
+        COKING.RB().ii(RecipeIngredient.of(ItemTags.COALS, 1)).io(GEM.get(CoalCoke, 1)).fo(Creosote.getLiquid(200)).add(600, 0);
+        //COKING.RB().ii(RecipeIngredient.of(Blocks.COAL_BLOCK, 1)).io(BLOCK.get(CoalCoke)).fo(Creosote.getLiquid(1800)).add(5400, 0);
     }
 }
