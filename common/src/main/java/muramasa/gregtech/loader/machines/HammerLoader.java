06602430a591007f7b23feb94c33717be7f6b713
Rundas <38040866+Rundas01@users.noreply.github.com>
2023-03-17 11:15:30 +0100

    added info to mat tree
    also added 2 to 1 conversion from copper to annealed copper and iron to wrought iron in the forge hammer to get those materials pre arc-furnace

--- common/src/main/java/muramasa/gregtech/loader/machines/HammerLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/HammerLoader.java
@@ -1,16 +1,19 @@
 package muramasa.gregtech.loader.machines;
 
-
-import static muramasa.antimatter.data.AntimatterMaterialTypes.INGOT;
-import static muramasa.antimatter.data.AntimatterMaterialTypes.PLATE;
+import static muramasa.antimatter.data.AntimatterMaterials.*;
+import static muramasa.antimatter.data.AntimatterMaterialTypes.*;
+import static muramasa.gregtech.data.Materials.AnnealedCopper;
+import static muramasa.gregtech.data.Materials.WroughtIron;
 import static muramasa.gregtech.data.RecipeMaps.HAMMERING;
 
 public class HammerLoader {
     public static void init() {
         PLATE.all().forEach(plate -> {
             if (!plate.has(INGOT)) return;
-
-            HAMMERING.RB().ii(INGOT.getMaterialIngredient(plate, 3)).io(PLATE.get(plate, 2)).add("plate_" + plate.getId(),plate.getMass()*2, 13);
+            HAMMERING.RB().ii(INGOT.getMaterialIngredient(plate, 3)).io(PLATE.get(plate, 2)).add("plate_" + plate.getId(),plate.getHardness()/20, 13);
         });
+        //Wrought Iron and Annealed Copper 2 to 1 (pre Arc Furnace)
+        HAMMERING.RB().ii(DUST.getMaterialIngredient(Iron, 2)).io(DUST.get(WroughtIron, 1)).add("wrought_iron_pre_arc",Iron.getHardness()/20, 10);
+        HAMMERING.RB().ii(DUST.getMaterialIngredient(Copper, 2)).io(DUST.get(AnnealedCopper, 1)).add("annealed_copper_pre_arc",AnnealedCopper.getHardness()/20, 10);
     }
 }
