f84fc9de98191465d037ee832e8e7f4ffd7b3b40
Rundas <38040866+Rundas01@users.noreply.github.com>
2023-03-17 14:01:12 +0100

    Update AlloyLoader.java
    added recipe for rubber dust

--- common/src/main/java/muramasa/gregtech/loader/machines/AlloyLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/AlloyLoader.java
@@ -1,19 +1,17 @@
 package muramasa.gregtech.loader.machines;
 
-import muramasa.antimatter.data.AntimatterMaterials;
-import muramasa.antimatter.material.Material;
 import muramasa.antimatter.material.MaterialStack;
 import muramasa.antimatter.material.MaterialTags;
-import muramasa.antimatter.recipe.ingredient.RecipeIngredient;
 import muramasa.gregtech.data.Materials;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.List;
 
 import static muramasa.antimatter.data.AntimatterMaterialTypes.*;
+import static muramasa.antimatter.data.AntimatterMaterials.*;
 import static muramasa.antimatter.material.MaterialTags.METAL;
 import static muramasa.antimatter.recipe.ingredient.RecipeIngredient.of;
+import static muramasa.gregtech.data.Materials.*;
 import static muramasa.gregtech.data.RecipeMaps.ALLOY_SMELTING;
 
 public class AlloyLoader {
@@ -34,10 +32,12 @@ public class AlloyLoader {
             if (firstIngot) ALLOY_SMELTING.RB().ii(of(INGOT.getMaterialTag(first.m),first.s),of(DUST.getMaterialTag(second.m),second.s)).io(new ItemStack(INGOT.get(t),cumulative)).add(t.getId() +"_ingot_3",100, 12);
             if (secondIngot) ALLOY_SMELTING.RB().ii(of(DUST.getMaterialTag(first.m),first.s),of(INGOT.getMaterialTag(second.m),second.s)).io(new ItemStack(INGOT.get(t),cumulative)).add(t.getId() +"_ingot_4",100, 12);
         });
-
-        ALLOY_SMELTING.RB().ii(of(DUST.getMaterialTag(Materials.Brick), 1), of(DUST.getMaterialTag(AntimatterMaterials.Sand), 2))
-                .io(DUST.get(Materials.Fireclay, 1)).add("fireclay",20, 10);
-
+        //Fireclay
+        ALLOY_SMELTING.RB().ii(of(DUST.getMaterialTag(Brick), 1), of(DUST.getMaterialTag(Sand), 2))
+                .io(DUST.get(Fireclay, 1)).add("fireclay",20, 10);
+        //pre Chemical Reactor Rubber
+        ALLOY_SMELTING.RB().ii(of(DUST.get(RawRubber), 1), of(DUST.getMaterialTag(Sulfur), 1))
+                .io(DUST.get(Rubber, 1)).add("rubber_via_alloy_smelter",20, 10);
         //Fissile Fuels
         FISSILE_FUEL.all().forEach(f -> ALLOY_SMELTING.RB().ii(of(INGOT.get(f), 1), of(INGOT.get(f))).io(FISSILE_FUEL.get(f)).add(f.getId() + "_fissile_fuel_smelting"));
 
