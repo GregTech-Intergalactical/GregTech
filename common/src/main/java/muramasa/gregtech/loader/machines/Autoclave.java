17e1cfe2736821529bd1b99bc1cdff29d02bff77
Rundas <38040866+Rundas01@users.noreply.github.com>
2023-03-20 11:06:17 +0100

    Update Autoclave.java
    duration was way too high

--- common/src/main/java/muramasa/gregtech/loader/machines/Autoclave.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/Autoclave.java
@@ -10,7 +10,16 @@ public class Autoclave {
     public static void init() {
         GEM.all().forEach(g -> {
             if(!g.has(DUST)) return;
-            AUTOCLAVING.RB().ii(of(DUST.get(g))).fi(Water.getLiquid(1000)).io(GEM.get(g)).add(g.getId()+"_autoclaving", g.getDensity(),8);
+            int dur;
+            long d = g.getDensity();
+            if(d <= 1000){
+                dur = 100;
+            }else if(d <= 2000){
+                dur = 200;
+            }else{
+                dur = 400;
+            }
+            AUTOCLAVING.RB().ii(of(DUST.get(g))).fi(Water.getLiquid(1000)).io(GEM.get(g)).add(g.getId()+"_autoclaving", dur,8);
         });
     }
 }
