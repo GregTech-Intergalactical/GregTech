161eb9f73b76679edcd908e700768cff581fba31
Rundas <38040866+Rundas01@users.noreply.github.com>
2023-03-23 14:16:39 +0100

    Update BendingLoader.java

--- common/src/main/java/muramasa/gregtech/loader/machines/BendingLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/BendingLoader.java
@@ -11,13 +11,11 @@ public class BendingLoader {
     public static void init() {
         AntimatterMaterialTypes.PLATE.all().forEach(t -> {
             if (!t.has(AntimatterMaterialTypes.INGOT)) return;
-            long duration = Math.max(t.getHardness(), 1);
-            BENDING.RB().ii(AntimatterMaterialTypes.INGOT.getMaterialIngredient(t,1),INT_CIRCUITS.get(1)).io(AntimatterMaterialTypes.PLATE.get(t,1)).add("plate_" + t.getId(),duration, 24);
+            BENDING.RB().ii(AntimatterMaterialTypes.INGOT.getMaterialIngredient(t,1),INT_CIRCUITS.get(1)).io(AntimatterMaterialTypes.PLATE.get(t,1)).add("plate_" + t.getId(),t.getHardness(), 24);
         });
         AntimatterMaterialTypes.FOIL.all().forEach(foil -> {
             if (!foil.has(AntimatterMaterialTypes.PLATE)) return;
-            long duration = Math.max(foil.getHardness(),1);
-            BENDING.RB().ii(AntimatterMaterialTypes.PLATE.getMaterialIngredient(foil,1), INT_CIRCUITS.get(1)).io(AntimatterMaterialTypes.FOIL.get(foil,4)).add("foil_" + foil.getId(),duration,24);
+            BENDING.RB().ii(AntimatterMaterialTypes.PLATE.getMaterialIngredient(foil,1), INT_CIRCUITS.get(1)).io(AntimatterMaterialTypes.FOIL.get(foil,4)).add("foil_" + foil.getId(),foil.getHardness(),24);
         });
         BENDING.RB().ii(AntimatterMaterialTypes.PLATE.getMaterialIngredient(Materials.Tin,2), INT_CIRCUITS.get(4)).io(GregTechData.CellTin.getDefaultInstance()).add("ten_cell",80,24);
         BENDING.RB().ii(AntimatterMaterialTypes.PLATE.getMaterialIngredient(Materials.Steel,2), INT_CIRCUITS.get(4)).io(GregTechData.CellSteel.getDefaultInstance()).add("steel_cell",80,96);
