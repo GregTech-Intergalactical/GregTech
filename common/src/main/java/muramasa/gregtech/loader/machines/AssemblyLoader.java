6847b615433242272d7fcd4539c3049607d746f3
Rundas <38040866+Rundas01@users.noreply.github.com>
2023-03-23 11:33:50 +0100

    finally figured out the frames

--- common/src/main/java/muramasa/gregtech/loader/machines/AssemblyLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/AssemblyLoader.java
@@ -6,6 +6,7 @@ import muramasa.antimatter.item.ItemBasic;
 import muramasa.antimatter.item.ItemCover;
 import muramasa.antimatter.machine.Tier;
 import muramasa.antimatter.material.Material;
+import muramasa.antimatter.material.MaterialTypeBlock;
 import muramasa.antimatter.pipe.PipeItemBlock;
 import muramasa.antimatter.pipe.PipeSize;
 import muramasa.antimatter.pipe.types.Cable;
@@ -33,71 +34,16 @@ import static muramasa.gregtech.data.TierMaps.*;
 
 public class AssemblyLoader {
     public static void init() {
+        batteries();
+        casings();
         cables();
         coils();
+        frames();
         misc();
         motors();
         pistons();
         rotors();
-        casings();
-        batteries();
         smdComponents();
-    }
-
-    private static void cables(){
-        AntimatterAPI.all(Wire.class,t -> {
-            Cable<?> cable = AntimatterAPI.get(Cable.class, "cable" + "_" + t.getMaterial().getId());
-            if (cable == null) return;
-            ImmutableSet<PipeSize> sizes = t.getSizes();
-            sizes.forEach(size -> {
-                Item wireItem = t.getBlockItem(size);
-                Item cableItem = cable.getBlockItem(size);
-                ASSEMBLING.RB().ii(of(wireItem,1), INT_CIRCUITS.get(size.getCableThickness())).fi(Rubber.getLiquid(size.getCableThickness()*16)).io(new ItemStack(cableItem,1)).add("cable_" + t.getMaterial().getId() + "_" + size.getId(),size.getCableThickness()* 20L,8);
-            });
-        });
-    }
-
-    private static void coils(){
-        addCoil(GregTech.get(BlockCoil.class, "coil_cupronickel"), WIRE_CUPRONICKEL.getBlockItem(PipeSize.SMALL), Tin, 1);
-        addCoil(GregTech.get(BlockCoil.class, "coil_kanthal"), WIRE_KANTHAL.getBlockItem(PipeSize.SMALL), AnnealedCopper, 2);
-        addCoil(GregTech.get(BlockCoil.class, "coil_nichrome"), WIRE_NICHROME.getBlockItem(PipeSize.SMALL), Kanthal, 3);
-        addCoil(GregTech.get(BlockCoil.class, "coil_tungstensteel"), WIRE_TUNGSTEN_STEEL.getBlockItem(PipeSize.SMALL), Nichrome, 4);
-        addCoil(GregTech.get(BlockCoil.class, "coil_hssg"), WIRE_HSSG.getBlockItem(PipeSize.SMALL), TungstenSteel, 5);
-        addCoil(GregTech.get(BlockCoil.class, "coil_naquadah"), WIRE_NAQUADAH.getBlockItem(PipeSize.SMALL), HSSG, 6);
-        addCoil(GregTech.get(BlockCoil.class, "coil_naquadah_alloy"), WIRE_NAQUADAH_ALLOY.getBlockItem(PipeSize.SMALL), Naquadah, 7);
-        addCoil(GregTech.get(BlockCoil.class, "coil_superconductor"), WIRE_SUPERCONDUCTOR.getBlockItem(PipeSize.SMALL), NaquadahAlloy, 8);
-    }
-
-    private static void misc(){
-        ASSEMBLING.RB().ii(of(ItemTags.PLANKS,8), INT_CIRCUITS.get(8)).io(new ItemStack(Items.CHEST,1)).add("chest",100,4);
-        ASSEMBLING.RB().ii(of(PLATES_IRON_ALUMINIUM, 2), of(Items.IRON_BARS, 2)).io(new ItemStack(GregTech.get(ItemCover.class, COVER_DRAIN.getId()))).add("drain",800, 16);
-    }
-
-    private static void motors(){
-        Arrays.stream(Tier.getStandard()).forEach(t -> {
-            Material magnet = (t == Tier.ULV || t == Tier.LV) ? IronMagnetic : (t == Tier.EV || t == Tier.IV ? NeodymiumMagnetic : SteelMagnetic);
-            ASSEMBLING.RB().ii(of(TIER_WIRES.get(t),4), of(ROD.get(TIER_MATERIALS.get(t)),2),
-                    of(ROD.get(magnet),1)
-                    , of(TIER_CABLES.get(t),2)).io(new ItemStack(GregTech.get(ItemBasic.class,"motor_"+t.getId()))).add("motor_"+t.getId(),150,16);
-        });
-    }
-
-    private static void pistons(){
-        Arrays.stream(Tier.getStandard()).forEach(t -> {
-            ASSEMBLING.RB().ii(of(TIER_CABLES.get(t),2),
-                            of(ROD.get(TIER_MATERIALS.get(t)),2),
-                            of(PLATE.get(TIER_MATERIALS.get(t)),3),
-                            of(GregTech.get(ItemBasic.class,"motor_"+t.getId()),1),
-                            of(GEAR.get(TIER_MATERIALS.get(t)),1))
-                    .io(new ItemStack(GregTech.get(ItemBasic.class,"piston_"+t.getId())))
-                    .add("piston_"+t.getId(),150,16);
-        });
-    }
-
-    private static void rotors(){
-        ROTOR.all().forEach(r -> {
-            ASSEMBLING.RB().ii(PLATE.getMaterialIngredient(r,4),SCREW.getMaterialIngredient(r,1),of(RING.get(Rubber,1))).fi(SolderingAlloy.getLiquid(144)).io(new ItemStack(ROTOR.get(r),1)).add(r.getId() + "_rotor",240,24);
-        });
     }
 
     private static void batteries() {
@@ -136,6 +82,75 @@ public class AssemblyLoader {
         addCasing(Lead, GregTech.get(BlockCasing.class,"casing_radiation_proof"));
     }
 
+    private static void cables(){
+        AntimatterAPI.all(Wire.class,t -> {
+            Cable<?> cable = AntimatterAPI.get(Cable.class, "cable" + "_" + t.getMaterial().getId());
+            if (cable == null) return;
+            ImmutableSet<PipeSize> sizes = t.getSizes();
+            sizes.forEach(size -> {
+                Item wireItem = t.getBlockItem(size);
+                Item cableItem = cable.getBlockItem(size);
+                ASSEMBLING.RB().ii(of(wireItem,1), INT_CIRCUITS.get(size.getCableThickness())).fi(Rubber.getLiquid(size.getCableThickness()*16)).io(new ItemStack(cableItem,1)).add("cable_" + t.getMaterial().getId() + "_" + size.getId(),size.getCableThickness()* 20L,8);
+            });
+        });
+    }
+
+    private static void coils(){
+        addCoil(GregTech.get(BlockCoil.class, "coil_cupronickel"), WIRE_CUPRONICKEL.getBlockItem(PipeSize.SMALL), Tin, 1);
+        addCoil(GregTech.get(BlockCoil.class, "coil_kanthal"), WIRE_KANTHAL.getBlockItem(PipeSize.SMALL), AnnealedCopper, 2);
+        addCoil(GregTech.get(BlockCoil.class, "coil_nichrome"), WIRE_NICHROME.getBlockItem(PipeSize.SMALL), Kanthal, 3);
+        addCoil(GregTech.get(BlockCoil.class, "coil_tungstensteel"), WIRE_TUNGSTEN_STEEL.getBlockItem(PipeSize.SMALL), Nichrome, 4);
+        addCoil(GregTech.get(BlockCoil.class, "coil_hssg"), WIRE_HSSG.getBlockItem(PipeSize.SMALL), TungstenSteel, 5);
+        addCoil(GregTech.get(BlockCoil.class, "coil_naquadah"), WIRE_NAQUADAH.getBlockItem(PipeSize.SMALL), HSSG, 6);
+        addCoil(GregTech.get(BlockCoil.class, "coil_naquadah_alloy"), WIRE_NAQUADAH_ALLOY.getBlockItem(PipeSize.SMALL), Naquadah, 7);
+        addCoil(GregTech.get(BlockCoil.class, "coil_superconductor"), WIRE_SUPERCONDUCTOR.getBlockItem(PipeSize.SMALL), NaquadahAlloy, 8);
+    }
+
+    private static void frames(){
+        FRAME.all().forEach(m -> {
+            MaterialTypeBlock.Container f = FRAME.get().get(m);
+            ASSEMBLING.RB().ii(of(ROD.get(m),6)).io(new ItemStack(f.asItem(),2)).add(AntimatterPlatformUtils.getIdFromBlock(f.asBlock()).getPath(),40,24);
+        });
+    }
+
+    private static void misc(){
+        ASSEMBLING.RB().ii(of(ItemTags.PLANKS,8), INT_CIRCUITS.get(8)).io(new ItemStack(Items.CHEST,1)).add("chest",100,4);
+        ASSEMBLING.RB().ii(of(PLATES_IRON_ALUMINIUM, 2), of(Items.IRON_BARS, 2)).io(new ItemStack(GregTech.get(ItemCover.class, COVER_DRAIN.getId()))).add("drain",800, 16);
+    }
+
+    private static void motors(){
+        Arrays.stream(Tier.getStandard()).forEach(t -> {
+            Material magnet = (t == Tier.ULV || t == Tier.LV) ? IronMagnetic : (t == Tier.EV || t == Tier.IV ? NeodymiumMagnetic : SteelMagnetic);
+            ASSEMBLING.RB().ii(of(TIER_WIRES.get(t),4), of(ROD.get(TIER_MATERIALS.get(t)),2),
+                    of(ROD.get(magnet),1)
+                    , of(TIER_CABLES.get(t),2)).io(new ItemStack(GregTech.get(ItemBasic.class,"motor_"+t.getId()))).add("motor_"+t.getId(),150,16);
+        });
+    }
+
+    private static void pistons(){
+        Arrays.stream(Tier.getStandard()).forEach(t -> {
+            ASSEMBLING.RB().ii(of(TIER_CABLES.get(t),2),
+                            of(ROD.get(TIER_MATERIALS.get(t)),2),
+                            of(PLATE.get(TIER_MATERIALS.get(t)),3),
+                            of(GregTech.get(ItemBasic.class,"motor_"+t.getId()),1),
+                            of(GEAR.get(TIER_MATERIALS.get(t)),1))
+                    .io(new ItemStack(GregTech.get(ItemBasic.class,"piston_"+t.getId())))
+                    .add("piston_"+t.getId(),150,16);
+        });
+    }
+
+    private static void rotors(){
+        ROTOR.all().forEach(r -> {
+            ASSEMBLING.RB().ii(PLATE.getMaterialIngredient(r,4),SCREW.getMaterialIngredient(r,1),of(RING.get(Rubber,1))).fi(SolderingAlloy.getLiquid(144)).io(new ItemStack(ROTOR.get(r),1)).add(r.getId() + "_rotor",240,24);
+        });
+    }
+
+    private static void smdComponents () {
+        ASSEMBLING.RB().ii(of(PLATE.get(Silicon), 1), of(WIRE_FINE.get(AnnealedCopper), 8)).fi(Polyethylene.getLiquid(288)).io(new ItemStack(Transistor,32)).add("transistor",80, 96);
+        ASSEMBLING.RB().ii(of(DUST.get(Carbon), 1), of(WIRE_FINE.get(Electrum), 4)).fi(Polyethylene.getLiquid(288)).io(new ItemStack(Resistor,24)).add("resistor",80, 96);
+        ASSEMBLING.RB().ii(of(FOIL.get(PolyvinylChloride), 4), of(FOIL.get(Aluminium), 2)).fi(Polyethylene.getLiquid(288)).io(new ItemStack(Capacitor,16)).add("capacitor",80, 96);
+    }
+
     private static void addTierCasing (Material mat, Material mat2, Block casing, int tier) {
         ASSEMBLING.RB().ii(of(PLATE.get(mat, 2)), of(BOLT.get(mat2, 2))).io(new ItemStack(casing)).add(AntimatterPlatformUtils.getIdFromBlock(casing).getPath(),5 * 20, (long) Math.pow(2, 2 * tier + 1));
     }
@@ -148,22 +163,11 @@ public class AssemblyLoader {
         ASSEMBLING.RB().ii(of(SCREW.get(mat), 2), of(casing, 1), of(Items.REDSTONE, 1), of(w.getBlockItem(PipeSize.VTINY), 1), of(TIER_CIRCUITS.getOrDefault(tier, circ), 1)).io(new ItemStack(hull)).add(AntimatterPlatformUtils.getIdFromBlock(hull).getPath() + "_" + idOffset,5 * 20, (long) Math.pow(2, 2 * tier + 1));
     }
 
-    private static void addFrame (Material mat, Block frame) {
-        ASSEMBLING.RB().ii(of(ROD.get(mat), 4), of(PLATE.get(mat), 4)).io(new ItemStack(frame,2)).add(AntimatterPlatformUtils.getIdFromBlock(frame).getPath(),40, 24);
-    }
-
     private static void addCasing (Material mat, BlockCasing casing) {
-        ASSEMBLING.RB().ii(of(ROD.get(mat), 4), of(PLATE.get(mat), 4)).io(new ItemStack(casing,2)).add(AntimatterPlatformUtils.getIdFromBlock(casing).getPath(),40, 24);
+        ASSEMBLING.RB().ii(of(FRAME.get().get(mat).asItem(), 1), of(PLATE.get(mat), 4)).io(new ItemStack(casing,2)).add(AntimatterPlatformUtils.getIdFromBlock(casing).getPath(),80, 30);
     }
 
     private static void addCoil (BlockCoil coil, PipeItemBlock wire, Material fluidmat, int tier) {
         ASSEMBLING.RB().ii(of(wire,8), of(DUST.get(Aluminosilicate), 4)).fi(fluidmat.getLiquid(288)).io(new ItemStack(coil,1)).add(AntimatterPlatformUtils.getIdFromBlock(coil).getPath(), 100*tier, (long) (30*Math.pow(4,tier-1)));
     }
-
-    private static void smdComponents () {
-        ASSEMBLING.RB().ii(of(PLATE.get(Silicon), 1), of(WIRE_FINE.get(AnnealedCopper), 8)).fi(Polyethylene.getLiquid(288)).io(new ItemStack(Transistor,32)).add("transistor",80, 96);
-        ASSEMBLING.RB().ii(of(DUST.get(Carbon), 1), of(WIRE_FINE.get(Electrum), 4)).fi(Polyethylene.getLiquid(288)).io(new ItemStack(Resistor,24)).add("resistor",80, 96);
-        ASSEMBLING.RB().ii(of(FOIL.get(PolyvinylChloride), 4), of(FOIL.get(Aluminium), 2)).fi(Polyethylene.getLiquid(288)).io(new ItemStack(Capacitor,16)).add("capacitor",80, 96);
-    }
-
 }
