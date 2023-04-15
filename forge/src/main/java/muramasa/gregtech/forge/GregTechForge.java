47ff3c550ba06ade94e0ba5f39279fb4bd94cf39
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-04-14 23:00:42 -0400

    tried renaming modid to gti, but only on fabric

--- forge/src/main/java/muramasa/gregtech/forge/GregTechForge.java
+++ forge/src/main/java/muramasa/gregtech/forge/GregTechForge.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(GTIRef.ID)
+@Mod("gregtech")
 public class GregTechForge {
 
     public GregTechForge(){
