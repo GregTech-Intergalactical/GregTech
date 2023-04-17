3262d327b7bcb7d5a8b34368977f774ac0a94c9d
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-04-16 20:28:40 -0400

    fixed macerator sound still being called pulverizer

--- common/src/main/java/muramasa/gregtech/data/GregTechSounds.java
+++ common/src/main/java/muramasa/gregtech/data/GregTechSounds.java
@@ -12,7 +12,7 @@ public class GregTechSounds {
     }
 
     public static final SoundEvent EXTRACTOR = AntimatterAPI.register(SoundEvent.class, GTIRef.ID, "extractor", new SoundEvent(new ResourceLocation(GTIRef.ID, "extractor")));
-    public static final SoundEvent PULVERIZER = AntimatterAPI.register(SoundEvent.class, GTIRef.ID, "pulverizer", new SoundEvent(new ResourceLocation(GTIRef.ID, "pulverizer")));
+    public static final SoundEvent MACERATOR = AntimatterAPI.register(SoundEvent.class, GTIRef.ID, "macerator", new SoundEvent(new ResourceLocation(GTIRef.ID, "macerator")));
     public static final SoundEvent MAGNETIZER = AntimatterAPI.register(SoundEvent.class, GTIRef.ID, "magnetizer", new SoundEvent(new ResourceLocation(GTIRef.ID, "magnetizer")));
     public static final SoundEvent FURNACE = AntimatterAPI.register(SoundEvent.class, GTIRef.ID, "furnace", new SoundEvent(new ResourceLocation(GTIRef.ID, "furnace")));
     public static final SoundEvent TREETAP = AntimatterAPI.register(SoundEvent.class, GTIRef.ID, "treetap", new SoundEvent(new ResourceLocation(GTIRef.ID, "treetap")));
