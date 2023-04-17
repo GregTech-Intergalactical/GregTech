cd360a6dd80af38419b9691ff7b75383a9a90736
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-04-16 22:10:27 -0400

    Modid rename (#181)
    * tried renaming modid to gti, but only on fabric
    
    * removed symlink
    
    * renamed modid from gregtech to gti, remapped all missing blocks, items, and tiles resulting from the rename
    
    * fixed modid of sounds
    
    * woops missed one sound

--- common/src/main/java/muramasa/gregtech/GTIRef.java
+++ common/src/main/java/muramasa/gregtech/GTIRef.java
@@ -1,11 +1,12 @@
 package muramasa.gregtech;
 
 import muramasa.antimatter.Ref;
+import muramasa.antimatter.util.AntimatterPlatformUtils;
 
 public class GTIRef {
 
     /** Mod Data **/
-    public static final String ID = "gregtech";
+    public static final String ID = "gti";
     public static final String NAME = "GregTech";
     public static final String VERSION = "0.0.1";
     public static final String ANTIMATTER = Ref.ID;
