ba0a4a4c38bfb40348a14da2a074ab5a297d6031
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-04-20 23:32:11 -0400

    added missing coke oven recipes

--- common/src/main/java/muramasa/gregtech/loader/multi/Coking.java
+++ common/src/main/java/muramasa/gregtech/loader/multi/Coking.java
@@ -13,6 +13,8 @@ public class Coking {
     public static void init() {
         COKING.RB().ii(RecipeIngredient.of(ItemTags.LOGS, 1)).io(GEM.get(Charcoal, 1)).fo(Creosote.getLiquid(100)).add("charcoal",600, 0);
         COKING.RB().ii(RecipeIngredient.of(ItemTags.COALS, 1)).io(GEM.get(CoalCoke, 1)).fo(Creosote.getLiquid(200)).add("coal_coke",600, 0);
-        //COKING.RB().ii(RecipeIngredient.of(Blocks.COAL_BLOCK, 1)).io(BLOCK.get(CoalCoke)).fo(Creosote.getLiquid(1800)).add(5400, 0);
+        COKING.RB().ii(RecipeIngredient.of(GEM.getMaterialTag(Lignite), 1)).io(GEM.get(LigniteCoke, 1)).fo(Creosote.getLiquid(200)).add("lignite_coal_coke",600, 0);
+        COKING.RB().ii(RecipeIngredient.of(Blocks.COAL_BLOCK, 1)).io(BLOCK.get().get(CoalCoke).asStack()).fo(Creosote.getLiquid(1800)).add("coal_coke_block",5400, 0);
+        COKING.RB().ii(RecipeIngredient.of(BLOCK.getMaterialTag(Lignite), 1)).io(BLOCK.get().get(LigniteCoke).asStack()).fo(Creosote.getLiquid(1800)).add("lignite_coal_coke_block",5400, 0);
     }
 }
