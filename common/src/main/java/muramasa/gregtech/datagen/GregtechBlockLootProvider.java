30bcf1c156a3604f6391675f537c2d8f4c997847
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-05-02 23:48:45 -0400

    added block loot for vanilla ores

--- common/src/main/java/muramasa/gregtech/datagen/GregtechBlockLootProvider.java
+++ common/src/main/java/muramasa/gregtech/datagen/GregtechBlockLootProvider.java
@@ -1,6 +1,7 @@
 package muramasa.gregtech.datagen;
 
 import muramasa.antimatter.AntimatterAPI;
+import muramasa.antimatter.data.AntimatterMaterialTypes;
 import muramasa.antimatter.datagen.providers.AntimatterBlockLootProvider;
 import muramasa.antimatter.material.Material;
 import muramasa.antimatter.ore.BlockOre;
@@ -10,12 +11,15 @@ import muramasa.gregtech.data.GregTechData;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
 import static muramasa.antimatter.Data.*;
+import static muramasa.antimatter.data.AntimatterMaterialTypes.RAW_ORE;
+import static muramasa.antimatter.data.AntimatterMaterials.*;
 
 public class GregtechBlockLootProvider extends AntimatterBlockLootProvider {
     public GregtechBlockLootProvider(String providerDomain, String providerName) {
@@ -27,5 +31,15 @@ public class GregtechBlockLootProvider extends AntimatterBlockLootProvider {
         super.loot();
         AntimatterAPI.all(BlockCasing.class,providerDomain, this::add);
         AntimatterAPI.all(BlockCoil.class,providerDomain, this::add);
+        tables.put(Blocks.LAPIS_ORE, b -> createOreDrop(b, RAW_ORE.get(Lapis)));
+        tables.put(Blocks.DEEPSLATE_LAPIS_ORE, b -> createOreDrop(b, RAW_ORE.get(Lapis)));
+        tables.put(Blocks.REDSTONE_ORE, b -> createOreDrop(b, RAW_ORE.get(Redstone)));
+        tables.put(Blocks.DEEPSLATE_REDSTONE_ORE, b -> createOreDrop(b, RAW_ORE.get(Redstone)));
+        tables.put(Blocks.DIAMOND_ORE, b -> createOreDrop(b, RAW_ORE.get(Diamond)));
+        tables.put(Blocks.DEEPSLATE_DIAMOND_ORE, b -> createOreDrop(b, RAW_ORE.get(Diamond)));
+        tables.put(Blocks.EMERALD_ORE, b -> createOreDrop(b, RAW_ORE.get(Emerald)));
+        tables.put(Blocks.DEEPSLATE_EMERALD_ORE, b -> createOreDrop(b, RAW_ORE.get(Emerald)));
+        tables.put(Blocks.COAL_ORE, b -> createOreDrop(b, RAW_ORE.get(Coal)));
+        tables.put(Blocks.DEEPSLATE_COAL_ORE, b -> createOreDrop(b, RAW_ORE.get(Coal)));
     }
 }
