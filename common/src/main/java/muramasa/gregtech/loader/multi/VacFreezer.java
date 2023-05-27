9e9f9e5784d23de0566ac4c5ca4e7c1ba9a66677
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-05-26 21:56:07 -0400

    fixed hot uranium and thorium isotopes not having a blast furnace temp

--- common/src/main/java/muramasa/gregtech/loader/multi/VacFreezer.java
+++ common/src/main/java/muramasa/gregtech/loader/multi/VacFreezer.java
@@ -8,13 +8,14 @@ import net.minecraft.world.item.ItemStack;
 
 import static muramasa.antimatter.data.AntimatterMaterialTypes.INGOT;
 import static muramasa.antimatter.data.AntimatterMaterialTypes.INGOT_HOT;
+import static muramasa.gregtech.data.GregTechMaterialTags.BLAST_FURNACE_TEMP;
 import static muramasa.gregtech.data.RecipeMaps.VACUUM_FREEZING;
 
 public class VacFreezer {
     public static void init() {
         INGOT_HOT.all().forEach(hi -> {
             Item ingot = INGOT.get(hi);
-            int temp = GregTechMaterialTags.BLAST_FURNACE_TEMP.getInt(hi);
+            int temp = BLAST_FURNACE_TEMP.getInt(hi);
             int voltage;
             if (temp < 1800) {
                 voltage = 120;
