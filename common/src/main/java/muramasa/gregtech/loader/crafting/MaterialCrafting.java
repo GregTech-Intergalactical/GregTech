b3c9ee10d218599de3c7c4c516b427d00e24a1f5
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-05-29 22:10:57 -0400

    added mortar recipe for clay dust

--- common/src/main/java/muramasa/gregtech/loader/crafting/MaterialCrafting.java
+++ common/src/main/java/muramasa/gregtech/loader/crafting/MaterialCrafting.java
@@ -1,5 +1,6 @@
 package muramasa.gregtech.loader.crafting;
 
+import com.google.common.collect.ImmutableMap;
 import muramasa.antimatter.data.AntimatterDefaultTools;
 import muramasa.antimatter.data.AntimatterMaterialTypes;
 import muramasa.antimatter.data.AntimatterMaterials;
@@ -7,14 +8,17 @@ import muramasa.antimatter.datagen.providers.AntimatterRecipeProvider;
 import muramasa.gregtech.GTIRef;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 import java.util.function.Consumer;
 
+import static muramasa.antimatter.data.AntimatterDefaultTools.MORTAR;
 import static muramasa.gregtech.data.Materials.*;
 
 public class MaterialCrafting {
     public static void loadRecipes(Consumer<FinishedRecipe> output, AntimatterRecipeProvider provider) {
         addShapelessDustRecipe(output, provider, "bronze_dust", AntimatterMaterialTypes.DUST.get(Bronze, 4), AntimatterMaterialTypes.DUST.get(AntimatterMaterials.Copper), AntimatterMaterialTypes.DUST.get(AntimatterMaterials.Copper), AntimatterMaterialTypes.DUST.get(AntimatterMaterials.Copper), AntimatterMaterialTypes.DUST.get(Tin));
+        provider.shapeless(output, GTIRef.ID, "", "dusts", "has_mortor", provider.hasSafeItem(MORTAR.getTag()), AntimatterMaterialTypes.DUST_SMALL.get(Clay, 2), MORTAR.getTag(), Items.CLAY_BALL);
     }
 
     private static void addShapelessDustRecipe(Consumer<FinishedRecipe> output, AntimatterRecipeProvider provider, String recipeName, ItemStack outputItem, Object... inputs)
