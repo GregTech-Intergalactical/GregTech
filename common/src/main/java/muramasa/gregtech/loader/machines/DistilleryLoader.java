f490e2c9436a0728dc7aee57b17c0b48e5e44bc8
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-06-21 00:33:52 -0400

    fixed distillery not doing all byproducts

--- common/src/main/java/muramasa/gregtech/loader/machines/DistilleryLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/DistilleryLoader.java
@@ -11,13 +11,14 @@ import static muramasa.gregtech.data.TierMaps.INT_CIRCUITS;
 public class DistilleryLoader {
     public static void init() {
         GregTechMaterialTags.DISTILL_INTO.getAll().forEach((material, fluidProducts) -> {
-            if(fluidProducts.size() > 1) return; // Use the distillation tower for materials with more than 1 product
-            Material fo = fluidProducts.get(0).mat();
-            DISTILLING.RB()
-                    .ii(INT_CIRCUITS.get(1).setNoConsume())
-                    .fi(material.has(AntimatterMaterialTypes.LIQUID) ? material.getLiquid(GregTechMaterialTags.DISTILLATION_FLUID_INPUT_AMOUNT.getInt(material)) : material.getGas(GregTechMaterialTags.DISTILLATION_FLUID_INPUT_AMOUNT.getInt(material)))
-                    .fo(fo.has(AntimatterMaterialTypes.LIQUID) ? fo.getLiquid(fluidProducts.get(0).amount()) : fo.getGas(fluidProducts.get(0).amount()))
-                    .add(material.getId(), material.getMass(), 20);
+            for (int i = 0; i < fluidProducts.size(); i++){
+                Material fo = fluidProducts.get(i).mat();
+                DISTILLING.RB()
+                        .ii(INT_CIRCUITS.get(i + 1).setNoConsume())
+                        .fi(material.has(AntimatterMaterialTypes.LIQUID) ? material.getLiquid(GregTechMaterialTags.DISTILLATION_FLUID_INPUT_AMOUNT.getInt(material)) : material.getGas(GregTechMaterialTags.DISTILLATION_FLUID_INPUT_AMOUNT.getInt(material)))
+                        .fo(fo.has(AntimatterMaterialTypes.LIQUID) ? fo.getLiquid(fluidProducts.get(i).amount()) : fo.getGas(fluidProducts.get(i).amount()))
+                        .add(material.getId(), material.getMass(), 20);
+            }
         });
     }
 }
