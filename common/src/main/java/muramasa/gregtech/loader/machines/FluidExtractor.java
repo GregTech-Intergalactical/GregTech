deaa74f2fda4f99b77ba87c2dc65d6dca268b55f
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-07-18 00:07:28 -0400

    fixed fluid extractor recipes to better match gt5u

--- common/src/main/java/muramasa/gregtech/loader/machines/FluidExtractor.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/FluidExtractor.java
@@ -8,43 +8,28 @@ import muramasa.antimatter.recipe.ingredient.RecipeIngredient;
 import muramasa.antimatter.util.AntimatterPlatformUtils;
 import net.minecraft.world.item.Item;
 
-import static muramasa.antimatter.Ref.L;
+import static muramasa.antimatter.Ref.*;
+import static muramasa.antimatter.data.AntimatterMaterialTypes.*;
 import static muramasa.gregtech.data.RecipeMaps.FLUID_EXTRACTING;
 
 public class FluidExtractor {
     public static void init() {
-        AntimatterMaterialTypes.ROD.all().forEach(r -> {
-            add(r, AntimatterMaterialTypes.ROD, 0.5f);
-        });
-        AntimatterMaterialTypes.PLATE.all().forEach(r -> {
-            add(r, AntimatterMaterialTypes.PLATE, 1f);
-        });
-        AntimatterMaterialTypes.INGOT.all().forEach(r -> {
-            add(r, AntimatterMaterialTypes.INGOT, 1f);
-        });
-        AntimatterMaterialTypes.SCREW.all().forEach(r -> {
-            add(r, AntimatterMaterialTypes.SCREW, 0.125f);
-        });
-        AntimatterMaterialTypes.GEAR.all().forEach(r -> {
-            add(r, AntimatterMaterialTypes.GEAR, 4f);
-        });
-        AntimatterMaterialTypes.FOIL.all().forEach(r -> {
-            add(r, AntimatterMaterialTypes.FOIL, 0.25f);
-        });
-        AntimatterMaterialTypes.BOLT.all().forEach(r -> {
-            add(r, AntimatterMaterialTypes.BOLT, 0.125f);
-        });
-        AntimatterMaterialTypes.PLATE_DENSE.all().forEach(r -> {
-            add(r, AntimatterMaterialTypes.PLATE_DENSE, 9f);
-        });
+        MaterialTypeItem<?>[] items = new MaterialTypeItem<?>[]{INGOT, NUGGET, PLATE, PLATE_DENSE, ROD, ROD_LONG, RING, FOIL, BOLT, SCREW, GEAR, GEAR_SMALL, WIRE_FINE, ROTOR};
+        for (MaterialTypeItem<?> item : items) {
+            item.all().forEach(m -> {
+                add(m, item, item.getUnitValue());
+            });
+        }
     }
 
-    private static void add(Material m, MaterialTypeItem<?> i, float ratio) {
+    private static void add(Material m, MaterialTypeItem<?> i, long materialAmount) {
         if (!m.has(AntimatterMaterialTypes.LIQUID)) return;
-        long amount = (long) (L * ratio);
+        long amount = //(long) (L * ratio);
+        (L * materialAmount) / U;
+        long duration = Math.max(1, (24 * materialAmount) / U);
         FLUID_EXTRACTING.RB()
                 .ii(RecipeIngredient.of(i.getMaterialTag(m),1))
                 .fo(m.getLiquid(amount))
-                .add(m.getId() + "_from_" + i.getId(), (long)(m.getMass()*((float)amount/L)), 64, MaterialTags.MELTING_POINT.getInt(m));
+                .add(m.getId() + "_from_" + i.getId(), (long)(m.getMass()*((float)amount/L)), Math.max(8, (int) Math.sqrt(2 * MaterialTags.MELTING_POINT.getInt(m))), duration);
     }
 }
