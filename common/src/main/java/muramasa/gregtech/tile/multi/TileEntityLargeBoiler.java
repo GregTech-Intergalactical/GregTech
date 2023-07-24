ca63da80567d209fa1e01284eea4c4fc0ce4a9be
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-07-24 01:42:21 -0400

    started work on large boilers

--- common/src/main/java/muramasa/gregtech/tile/multi/TileEntityLargeBoiler.java
+++ common/src/main/java/muramasa/gregtech/tile/multi/TileEntityLargeBoiler.java
@@ -1,15 +1,24 @@
 package muramasa.gregtech.tile.multi;
 
+import muramasa.antimatter.gui.SlotType;
 import muramasa.antimatter.machine.types.Machine;
 import muramasa.antimatter.tile.multi.TileEntityMultiMachine;
 import muramasa.gregtech.data.GregTechData;
+import muramasa.gregtech.items.ItemIntCircuit;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
 import static muramasa.antimatter.machine.Tier.*;
 
 public class TileEntityLargeBoiler extends TileEntityMultiMachine<TileEntityLargeBoiler> {
+
+    private boolean firstRun = true;
+    private int mSuperEfficencyIncrease = 0;
+    private int integratedCircuitConfig = 0; //Steam output is reduced by 1000L per config
+    private int excessFuel = 0; //Eliminate rounding errors for fuels that burn half items
+    private int excessProjectedEU = 0; //Eliminate rounding errors from throttling the boiler
 
     public TileEntityLargeBoiler(Machine<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
@@ -48,4 +57,19 @@ public class TileEntityLargeBoiler extends TileEntityMultiMachine<TileEntityLarg
         return GregTechData.CASING_PIPE_TUNGSTENSTEEL;
     }
 
+    public boolean checkRecipe(ItemStack stack){
+        itemHandler.ifPresent(i -> {
+            ItemStack circuit = i.getHandler(SlotType.STORAGE).getItem(0);
+            if (circuit.getItem() instanceof ItemIntCircuit intCircuit){
+                if (intCircuit.circuitId > 0 && intCircuit.circuitId <= 24){
+                    integratedCircuitConfig = intCircuit.circuitId;
+                }
+            } else {
+                if (integratedCircuitConfig != 0){
+                    integratedCircuitConfig = 0;
+                }
+            }
+        });
+        return true;
+    }
 }
