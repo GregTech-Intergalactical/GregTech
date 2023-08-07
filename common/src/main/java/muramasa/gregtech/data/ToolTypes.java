6307fb8cda3e692310636a6f8ffe57b478a7b964
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-08-07 18:36:20 -0400

    removed line that was disableing the 3x3 drill mode

--- common/src/main/java/muramasa/gregtech/data/ToolTypes.java
+++ common/src/main/java/muramasa/gregtech/data/ToolTypes.java
@@ -80,16 +80,12 @@ public class ToolTypes {
     }
 
     public static void init(){
-        //temp method till this behavior has a hotkey
-        AntimatterDefaultTools.DRILL.removeBehaviour("aoe_break");
         AntimatterDefaultTools.DRILL.setBrokenItems(ImmutableMap.of("drill_lv", i -> getBrokenItem(i, PowerUnitLV), "drill_mv", i -> getBrokenItem(i, PowerUnitMV), "drill_hv", i -> getBrokenItem(i, PowerUnitHV)));
         AntimatterDefaultTools.CHAINSAW.setBrokenItems(ImmutableMap.of("chainsaw_lv", i -> getBrokenItem(i, PowerUnitLV), "chainsaw_mv", i -> getBrokenItem(i, PowerUnitMV), "chainsaw_hv", i -> getBrokenItem(i, PowerUnitHV)));
         AntimatterDefaultTools.ELECTRIC_WRENCH.setBrokenItems(ImmutableMap.of("electric_wrench_lv", i -> getBrokenItem(i, PowerUnitLV), "electric_wrench_mv", i -> getBrokenItem(i, PowerUnitMV), "electric_wrench_hv", i -> getBrokenItem(i, PowerUnitHV)));
         AntimatterDefaultTools.BUZZSAW.setBrokenItems(ImmutableMap.of("buzzsaw_lv", i -> getBrokenItem(i, PowerUnitLV), "buzzsaw_mv", i -> getBrokenItem(i, PowerUnitMV), "buzzsaw_hv", i -> getBrokenItem(i, PowerUnitHV)));
         AntimatterDefaultTools.ELECTRIC_SCREWDRIVER.setBrokenItems(ImmutableMap.of("electric_screwdriver_lv", i -> getBrokenItem(i, SmallPowerUnit)));
         AntimatterDefaultTools.JACKHAMMER.setBrokenItems(ImmutableMap.of("jackhammer_hv", i -> getBrokenItem(i, PowerUnitHV)));
-        AntimatterDefaultTools.WRENCH.addEffectiveBlocks(Blocks.HOPPER);
-        AntimatterDefaultTools.ELECTRIC_WRENCH.addEffectiveBlocks(Blocks.HOPPER);
     }
 
     private static ItemStack getBrokenItem(ItemStack tool, ItemLike broken){
