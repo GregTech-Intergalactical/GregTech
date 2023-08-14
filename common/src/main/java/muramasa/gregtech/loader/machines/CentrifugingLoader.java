4d9214e576816012b17a01e5b908f31c0bfa4392
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-08-13 22:14:04 -0400

    changed chance for cobalt 60 tiny dust

--- common/src/main/java/muramasa/gregtech/loader/machines/CentrifugingLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/CentrifugingLoader.java
@@ -67,7 +67,7 @@ public class CentrifugingLoader {
         CENTRIFUGING.RB().ii(of(GTRubberData.RUBBER_LOGS)).io(new ItemStack(GTRubberData.StickyResin), new ItemStack(GregTechData.PlantBall), DUST.get(Carbon, 1), DUST.get(Wood, 1)).fo(Methane.getGas(60)).chances(0.5, 0.375, 0.25, 0.25).add("rubber_logs", 200, 20);
 
         CENTRIFUGING.RB().ii(of(GTRubberData.StickyResin, 1)).io(DUST.get(RawRubber, 3), new ItemStack(GregTechData.PlantBall)).fo(Glue.getLiquid(100)).chances(10000, 1000).add("sticky_resin", 300, 5);
-        CENTRIFUGING.RB().ii(CRUSHED_REFINED.getMaterialIngredient(Cobalt, 1)).io(DUST.get(Cobalt, 1), DUST_TINY.get(Cobalt60, 1)).chances(1.0, 0.5).add("refined_cobalt_ore", 512, 32);
+        CENTRIFUGING.RB().ii(CRUSHED_REFINED.getMaterialIngredient(Cobalt, 1)).io(DUST.get(Cobalt, 1), DUST_TINY.get(Cobalt60, 1)).chances(1.0, 0.239).add("refined_cobalt_ore", 512, 32);
         //Methane
         addMethaneRecipe(MELON, 9, 72);
         addMethaneRecipe(BREAD, 9, 72);
