979b783baab48c5d906093ac2d44325a4cefab55
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-09-03 02:42:09 -0400

    updated deps, removed amethyst shards from amethyst gem tag

--- common/src/main/java/muramasa/gregtech/datagen/GregTechItemTagProvider.java
+++ common/src/main/java/muramasa/gregtech/datagen/GregTechItemTagProvider.java
@@ -40,6 +40,8 @@ public class GregTechItemTagProvider  extends AntimatterItemTagProvider {
         this.tag(TRANSISTORS).add(Transistor, SMDTransistor);
         this.tag(DIODES).add(Diode, SMDDiode);
         this.tag(GEM.getMaterialTag(Amethyst)).remove(Items.AMETHYST_SHARD);
+        this.tag(GEM.getTag()).remove(Items.AMETHYST_SHARD);
+        this.tag(BLOCK.getMaterialTag(Amethyst)).remove(Items.AMETHYST_BLOCK);
         this.tag(GEM.getMaterialTag(Quartz)).add(GEM.get(MilkyQuartz));
         this.tag(PLATES_IRON_ALUMINIUM).addTag(PLATE.getMaterialTag(Iron)).addTag(PLATE.getMaterialTag(WroughtIron)).addTag(PLATE.getMaterialTag(Aluminium));
         this.tag(DUST_LAPIS_LAZURITE).addTag(DUST.getMaterialTag(Lapis)).addTag(DUST.getMaterialTag(Lazurite));
