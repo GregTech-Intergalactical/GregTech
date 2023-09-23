00fa51e7c8f80087995bfd1a2ee4fb0b3791f146
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-09-22 22:56:28 -0400

    made rubber rings unbendable

--- common/src/main/java/muramasa/gregtech/loader/machines/BendingLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/BendingLoader.java
@@ -16,13 +16,13 @@ public class BendingLoader {
             BENDING.RB().ii(AntimatterMaterialTypes.INGOT.getMaterialIngredient(t,1),INT_CIRCUITS.get(1)).io(PLATE.get(t,1)).add("plate_" + t.getId(),t.getMass(), 24);
         });
         AntimatterMaterialTypes.FOIL.all().forEach(foil -> {
-            if (!foil.has(PLATE)) return;
+            if (!foil.has(PLATE) || foil.has(MaterialTags.RUBBERTOOLS)) return;
             BENDING.RB().ii(PLATE.getMaterialIngredient(foil,1), INT_CIRCUITS.get(1)).io(AntimatterMaterialTypes.FOIL.get(foil,4)).add("foil_" + foil.getId(),foil.getMass(),24);
         });
-        AntimatterMaterialTypes.SPRING.all().forEach(spring -> {
+        AntimatterMaterialTypes.SPRING.all().stream().filter(m -> !m.has(MaterialTags.RUBBERTOOLS)).forEach(spring -> {
             BENDING.RB().ii(ROD_LONG.getMaterialIngredient(spring, 1), INT_CIRCUITS.get(1)).io(SPRING.get(spring)).add(spring.getId() + "_spring", 200, 16);
         });
-        RING.all().forEach(ring -> {
+        RING.all().stream().filter(m -> !m.has(MaterialTags.RUBBERTOOLS)).forEach(ring -> {
             BENDING.RB().ii(ROD.getMaterialIngredient(ring, 1), INT_CIRCUITS.get(1)).io(RING.get(ring, 2)).add(ring.getId() + "_ring", 200, 16);
         });
         BENDING.RB().ii(PLATE.getMaterialIngredient(Materials.Tin,2), INT_CIRCUITS.get(4)).io(GregTechData.CellTin.getDefaultInstance()).add("tin_cell",80,24);
