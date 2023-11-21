c86e94e2399f50d8540b48d95eec2910e154d1bc
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-11-21 12:15:38 -0500

    made cold coolant recipes require distilled water

--- common/src/main/java/muramasa/gregtech/loader/machines/MixerLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/MixerLoader.java
@@ -117,7 +117,7 @@ public class MixerLoader {
         MIXING.RB().ii(of(DUST.get(AntimatterMaterials.Stone,1))).fi(Lubricant.getLiquid(20), AntimatterMaterials.Water.getLiquid(4980)).fo(DrillingFluid.getLiquid(5000)).add("drilling_fluid",32*2, 16);
         MIXING.RB().ii(of(DUST.get(AntimatterMaterials.Stone,3)),of(DUST.get(Clay,1))).fi(AntimatterMaterials.Water.getLiquid(500)).fo(Concrete.getLiquid(576)).add("concrete", 20, 16);
         MIXING.RB().ii(RecipeIngredient.of(GTCoreItems.Biochaff)).fi(AntimatterMaterials.Water.getLiquid(1000)).fo(Biomass.getLiquid(1000)).add("biomass", 400, 8);
-        MIXING.RB().ii(DUST.getMaterialIngredient(Lapis, 1)).fi(Water.getLiquid(125)).fo(Coolant.getLiquid(125)).add("cold_coolant", 256, 48);
+        MIXING.RB().ii(DUST.getMaterialIngredient(Lapis, 1)).fi(DistilledWater.getLiquid(125)).fo(Coolant.getLiquid(125)).add("cold_coolant", 256, 48);
         long oneThird = AntimatterPlatformUtils.isFabric() ? 27000 : 333;
         MIXING.RB().ii(DUST.getMaterialIngredient(Wood, 4), DUST.getMaterialIngredient(Sulfur, 1), DUST.getMaterialIngredient(Lithium, 1)).fi(Glue.getLiquid(oneThird)).io(new ItemStack(SuperFuelBinder, 8)).add("super_fuel_binder", 40 * 20, 16);
         MIXING.RB().ii(DUST.getMaterialIngredient(Wood, 4), DUST.getMaterialIngredient(Sulfur, 1), DUST.getMaterialIngredient(Sodium, 1)).fi(Glue.getLiquid(oneThird)).io(new ItemStack(SuperFuelBinder, 8)).add("super_fuel_binder_1", 40 * 20, 16);
