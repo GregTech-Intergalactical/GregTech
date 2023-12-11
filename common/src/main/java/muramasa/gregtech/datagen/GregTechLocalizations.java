d5c7cbe7307f1ecdf7d07feef97e0fce8110433b
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-12-11 00:30:35 -0500

    expanded lang fix to stuff like pistons and emitters

--- common/src/main/java/muramasa/gregtech/datagen/GregTechLocalizations.java
+++ common/src/main/java/muramasa/gregtech/datagen/GregTechLocalizations.java
@@ -78,7 +78,7 @@ public class GregTechLocalizations {
             AntimatterAPI.all(BlockColoredWall.class, domain).forEach(i -> add(i, lowerUnderscoreToUpperSpaced(i.getId())));
             AntimatterAPI.all(BlockCoil.class, domain).forEach(i -> add(i, lowerUnderscoreToUpperSpaced(i.getId())));
             AntimatterAPI.all(ItemIntCircuit.class, domain).forEach(i -> override(i.getDescriptionId(), "Integrated Circuit (" + i.circuitId + ")"));
-            AntimatterAPI.all(ItemCover.class, domain).stream().filter(i -> i.getTier() != null).forEach(i -> override(i.getDescriptionId(), lowerUnderscoreToUpperSpaced(i.getId())
+            AntimatterAPI.all(ItemBasic.class, domain).forEach(i -> override(i.getDescriptionId(), lowerUnderscoreToUpperSpaced(i.getId())
                     .replace("Lv", "(LV)")
                     .replace("Mv", "(MV)")
                     .replace("Hv", "(HV)")
