9cc990b6b43b6a74d2b42ec54abbdc46a515005e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-12-12 13:48:17 -0500

    expanded crop harvester to a 9x9 instead of a 3x3

--- common/src/main/java/muramasa/gregtech/blockentity/single/BlockEntityCropHarvester.java
+++ common/src/main/java/muramasa/gregtech/blockentity/single/BlockEntityCropHarvester.java
@@ -8,24 +8,16 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BlockEntityCropHarvester extends BlockEntityMachine<BlockEntityCropHarvester> {
-    List<BlockPos> positions = new ArrayList<>();
+    AABB boundingBox;
     public BlockEntityCropHarvester(Machine<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
-        BlockPos down = pos.below();
-        positions.add(down);
-        positions.add(down.offset(-1, 0, 0));
-        positions.add(down.offset(1, 0, 0));
-        positions.add(down.offset(-1, 0, -1));
-        positions.add(down.offset(1, 0, -1));
-        positions.add(down.offset(-1, 0, 1));
-        positions.add(down.offset(1, 0, 1));
-        positions.add(down.offset(0, 0, -1));
-        positions.add(down.offset(0, 0, 1));
+        boundingBox = new AABB(pos.offset(4,-1,4), pos.offset(-4,-1,-4));
     }
 
     @Override
@@ -37,7 +29,7 @@ public class BlockEntityCropHarvester extends BlockEntityMachine<BlockEntityCrop
     public void serverTick(Level level, BlockPos pos, BlockState state) {
         super.serverTick(level, pos, state);
         if (isServerSide() && level.getGameTime() % 100 == 0) {
-            positions.forEach(p -> {
+            BlockPos.betweenClosedStream(boundingBox).forEach(p -> {
                 BlockState blockState = level.getBlockState(p);
                 if (blockState.getBlock() instanceof CropBlock cropBlock){
                     int age = blockState.getValue(cropBlock.getAgeProperty());
