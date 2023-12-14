d9fd74a05cd0e2060bf2b95de13aac5ff79ffb8d
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-12-14 00:28:06 -0500

    fixed slots in fluid press

--- common/src/main/java/muramasa/gregtech/data/Guis.java
+++ common/src/main/java/muramasa/gregtech/data/Guis.java
@@ -139,8 +139,8 @@ public class Guis {
         AMP_FABRICATOR.add(COMPRESSOR).add(FL_IN, 53, 63).add(FL_OUT, 107, 63);
         REPLICATOR.add(FLUID_CANNER);
         FERMENTER.add(FLUID_CANNER);
-        FLUID_PRESS.add(COMPRESSOR);
-        SMELTER.add(IT_IN, 53, 25).add(ENERGY, 80, 63).add(FL_OUT, 107, 63);
+        FLUID_PRESS.add(IT_IN, 53, 25).add(ENERGY, 80, 63).add(FL_OUT, 107, 63);
+        SMELTER.add(FLUID_PRESS);
         FLUID_HEATER.add(ENERGY, 80, 63).add(FL_IN, 53, 63).add(FL_OUT, 107, 63);
         FLUID_SOLIDIFIER.add(COMPRESSOR).add(FL_IN, 53, 63);
         SCANNER.add(FLUID_SOLIDIFIER).add(IT_IN, 125, 63);
