54d4b35f54e384bc04c8c5b89e3dac9be1688dc9
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-01-07 00:49:43 -0500

    added static int arrays to reactor core tile

--- common/src/main/java/muramasa/gregtech/blockentity/single/BlockEntityNuclearReactorCore.java
+++ common/src/main/java/muramasa/gregtech/blockentity/single/BlockEntityNuclearReactorCore.java
@@ -19,6 +19,8 @@ public class BlockEntityNuclearReactorCore extends BlockEntityMachine<BlockEntit
     public int[] oNeutronCounts = new int[]{0, 0, 0, 0};
     public long mEnergy = 0, oEnergy = 0;
     public byte mMode = 0;
+    public static final int[] S2103 = new int[] {0,0,2,1,0,3,0}, S0312 = new int[] {0,0,0,3,1,2,0};
+
     public BlockEntityNuclearReactorCore(Machine<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
         this.fluidHandler.set(() -> new MachineFluidHandler<>(this, 64000, 1000, 1, 1));
@@ -28,6 +30,10 @@ public class BlockEntityNuclearReactorCore extends BlockEntityMachine<BlockEntit
         return itemHandler.map(i -> i.getHandler(SlotType.STORAGE).getItem(slot)).orElse(ItemStack.EMPTY);
     }
 
+    // 0 and 2 are at SIDE_Z_NEG(North)    1 3      -->X+
+    // 1 and 3 are at SIDE_Z_POS(South)  2|0 2|0   |0 2
+    // 0 and 1 are at SIDE_X_NEG(West)   3|1 3|1   v1 3
+    // 2 and 3 are at SIDE_X_POS(East)     0 2     Z+
     public boolean isReactorRodModerated(int aSlot) {
 
         if (getRod(aSlot).getItem() instanceof IItemReactorRod reactorRod) {
