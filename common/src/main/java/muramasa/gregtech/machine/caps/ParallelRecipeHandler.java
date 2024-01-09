60f4ed41cea3f3258125d9ad7860c346734d2bd0
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-01-09 00:23:22 -0500

    fixed parrallel recipe handler not doing fluids properly

--- common/src/main/java/muramasa/gregtech/machine/caps/ParallelRecipeHandler.java
+++ common/src/main/java/muramasa/gregtech/machine/caps/ParallelRecipeHandler.java
@@ -1,5 +1,6 @@
 package muramasa.gregtech.machine.caps;
 
+import earth.terrarium.botarium.common.fluid.base.FluidHolder;
 import muramasa.antimatter.blockentity.BlockEntityMachine;
 import muramasa.antimatter.capability.machine.MachineRecipeHandler;
 import muramasa.antimatter.machine.event.MachineEvent;
@@ -41,22 +42,38 @@ public class ParallelRecipeHandler<T extends BlockEntityMachine<T>> extends Mach
                     }
                 });
             }
+            if (activeRecipe.hasOutputFluids()) {
+                tile.fluidHandler.ifPresent(h -> {
+                    h.addOutputs(activeRecipe.getOutputFluids());
+                });
+            }
         }
-        tile.onMachineEvent(MachineEvent.ITEMS_OUTPUTTED);
+        if (activeRecipe.hasOutputItems()) tile.onMachineEvent(MachineEvent.ITEMS_OUTPUTTED);
+        if (activeRecipe.hasOutputFluids()) tile.onMachineEvent(MachineEvent.FLUIDS_OUTPUTTED);
     }
 
     public boolean canOutput() {
         if (concurrentRecipes <= 1) return super.canOutput();
-        //ignore chance for canOutput.
-        if (!tile.itemHandler.isPresent() || !activeRecipe.hasOutputItems()) return true;
-        List<ItemStack> outputs = new ArrayList<>();
+        if (tile.itemHandler.isPresent() && activeRecipe.hasOutputItems() && !tile.itemHandler.map(t -> {
+            List<ItemStack> outputs = new ArrayList<>();
+            for (int i = 0; i < concurrentRecipes; i++) {
+                for (ItemStack item : activeRecipe.getFlatOutputItems()) {
+                    outputs.add(item.copy());
+                }
+            }
+            List<ItemStack> merged = Utils.mergeItems(new ArrayList<>(), outputs);
+            return t.canOutputsFit(merged.toArray(ItemStack[]::new));
+        }).orElse(false))
+            return false;
+        if (!tile.fluidHandler.isPresent() || !activeRecipe.hasOutputFluids()) return true;
+        List<FluidHolder> outputs = new ArrayList<>();
         for (int i = 0; i < concurrentRecipes; i++) {
-            for (ItemStack item : activeRecipe.getFlatOutputItems()) {
-                outputs.add(item.copy());
+            for (FluidHolder fluidHolder : activeRecipe.getOutputFluids()) {
+                outputs.add(fluidHolder.copyHolder());
             }
         }
-        List<ItemStack> merged = Utils.mergeItems(new ArrayList<>(), outputs);
-        return tile.itemHandler.map(t -> t.canOutputsFit(merged.toArray(ItemStack[]::new))).orElse(false);
+        List<FluidHolder> merged = Utils.mergeFluids(new ArrayList<>(), outputs);
+        return tile.fluidHandler.map(t -> t.canOutputsFit(merged.toArray(FluidHolder[]::new))).orElse(false);
     }
 
     protected int maxSimultaneousRecipes(){
