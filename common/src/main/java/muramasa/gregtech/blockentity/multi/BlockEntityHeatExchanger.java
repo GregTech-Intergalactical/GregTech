9973582853ef870bece3b1f09a788e404e32b8a1
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-01-09 00:23:43 -0500

    made heat exchanger function like gt5's mostly

--- common/src/main/java/muramasa/gregtech/blockentity/multi/BlockEntityHeatExchanger.java
+++ common/src/main/java/muramasa/gregtech/blockentity/multi/BlockEntityHeatExchanger.java
@@ -2,18 +2,27 @@ package muramasa.gregtech.blockentity.multi;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import muramasa.antimatter.blockentity.multi.BlockEntityMultiMachine;
+import muramasa.antimatter.capability.IFilterableHandler;
+import muramasa.antimatter.capability.fluid.FluidTank;
 import muramasa.antimatter.capability.machine.DefaultHeatHandler;
-import muramasa.antimatter.capability.machine.MachineRecipeHandler;
 import muramasa.antimatter.gui.GuiInstance;
 import muramasa.antimatter.gui.ICanSyncData;
 import muramasa.antimatter.gui.IGuiElement;
+import muramasa.antimatter.gui.SlotType;
 import muramasa.antimatter.gui.widget.InfoRenderWidget;
 import muramasa.antimatter.gui.widget.WidgetSupplier;
 import muramasa.antimatter.integration.jeirei.renderer.IInfoRenderer;
+import muramasa.antimatter.machine.event.IMachineEvent;
 import muramasa.antimatter.machine.event.MachineEvent;
 import muramasa.antimatter.machine.types.Machine;
+import muramasa.antimatter.material.Material;
+import muramasa.antimatter.util.Utils;
+import muramasa.gregtech.data.Materials;
+import muramasa.gregtech.items.ItemIntCircuit;
+import muramasa.gregtech.machine.caps.ParallelRecipeHandler;
 import net.minecraft.client.gui.Font;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -21,9 +30,155 @@ import tesseract.TesseractGraphWrappers;
 import tesseract.api.heat.IHeatHandler;
 
 import static muramasa.antimatter.data.AntimatterMaterials.Water;
-import static muramasa.gregtech.data.Materials.Steam;
+import static muramasa.gregtech.data.Materials.*;
 
-public class BlockEntityHeatExchanger extends BlockEntityMultiMachine<BlockEntityHeatExchanger> {
+public class BlockEntityHeatExchanger extends BlockEntityMultiMachine<BlockEntityHeatExchanger> implements IFilterableHandler {
+
+    int superheatedThreshold = 80000;
+    int efficiency = 1000;
+    int dryHeatCounter = 0;
+    int dryHeatMaximum = 100;
+
+    public BlockEntityHeatExchanger(Machine type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+        heatHandler.set(() -> new DefaultHeatHandler(this, Integer.MAX_VALUE, 80, 0));
+        recipeHandler.set(() -> new ParallelRecipeHandler<>(this){
+
+            @Override
+            protected int maxSimultaneousRecipes() {
+                if (activeRecipe != null){
+                    int base = 100;
+                    long totalHu = activeRecipe.getTotalPower();
+                    if (totalHu == 0) return 0;
+                    double ratio = 80.0 / totalHu;
+                    int totalRecipes = (int) (base * ratio);
+                    return Math.min(1, totalRecipes);
+                }
+                return super.maxSimultaneousRecipes();
+            }
+
+            @Override
+            protected boolean canRecipeContinue() {
+                return super.canRecipeContinue() && heatHandler.map(h -> h.getHeat() + (activeRecipe.getTotalPower()) <= h.getHeatCap()).orElse(false);
+            }
+
+            @Override
+            public boolean canOutput() {
+                return !tile.fluidHandler.isPresent() || !activeRecipe.hasOutputFluids() || tile.fluidHandler.map(t -> t.getOutputTanks() != null && t.getOutputTanks().getSize() > 1 && t.getOutputTanks().getTank(0).internalInsert(Utils.ca(activeRecipe.getOutputFluids()[0].getFluidAmount() * concurrentRecipes, activeRecipe.getOutputFluids()[0]), true) == activeRecipe.getOutputFluids()[0].getFluidAmount() * concurrentRecipes).orElse(false);
+            }
+
+            @Override
+            public boolean consumeResourceForRecipe(boolean simulate) {
+                return heatHandler.map(h -> h.insert((int) activeRecipe.getPower() * concurrentRecipes, simulate) > 0).orElse(false);
+            }
+
+            protected void addOutputs() {
+                for (int i = 0; i < concurrentRecipes; i++) {
+                    if (activeRecipe.hasOutputItems()) {
+                        tile.itemHandler.ifPresent(h -> {
+                            //Roll the chances here. If they don't fit add flat (no chances).
+                            ItemStack[] out = activeRecipe.getOutputItems(true);
+                            if (h.canOutputsFit(out)) {
+                                h.addOutputs(out);
+                            } else {
+                                h.addOutputs(activeRecipe.getFlatOutputItems());
+                            }
+                        });
+                    }
+                    if (activeRecipe.hasOutputFluids()) {
+                        tile.fluidHandler.ifPresent(h -> {
+                            if (h.getOutputTanks() == null) return;
+                            h.getOutputTanks().getTank(0).internalInsert(activeRecipe.getOutputFluids()[0].copyHolder(), false);
+                        });
+                    }
+                }
+                if (activeRecipe.hasOutputItems()) tile.onMachineEvent(MachineEvent.ITEMS_OUTPUTTED);
+                if (activeRecipe.hasOutputFluids()) tile.onMachineEvent(MachineEvent.FLUIDS_OUTPUTTED);
+            }
+        });
+
+    }
+
+    @Override
+    public void serverTick(Level level, BlockPos pos, BlockState state) {
+        super.serverTick(level, pos, state);
+        if (level.getGameTime() % 20 == 0){
+            fluidHandler.ifPresent(f -> {
+                heatHandler.ifPresent(h -> {
+                    if (h.getHeat() >= 80){
+                        int heatMultiplier = h.getHeat() / 80;
+                        FluidTank waterTank = f.getInputTanks().getTank(f.getInputTanks().getFirstAvailableTank(DistilledWater.getLiquid(1), true));
+                        if (waterTank != null) {
+                            heatMultiplier = (int) Math.min(heatMultiplier, waterTank.getTankAmount() / TesseractGraphWrappers.dropletMultiplier);
+                            if (waterTank.extractFluid(DistilledWater.getLiquid(heatMultiplier), true).getFluidAmount() == heatMultiplier *  TesseractGraphWrappers.dropletMultiplier) {
+                                if (f.getOutputTanks() != null && f.getOutputTanks().getSize() >= 2){
+                                    Material steam = Steam;
+                                    if (h.getHeat() >= superheatedThreshold){
+                                        steam = SuperheatedSteam;
+                                    }
+                                    float tEfficiency = steam == Steam ? 1 : efficiency / 1000.0f;
+                                    int waterMultiplier = steam == Steam ? 160 : 80;
+                                    int steamToAdd = (int) (heatMultiplier  * waterMultiplier *  tEfficiency);
+                                    long inserted = f.getOutputTanks().getTank(1).internalInsert(steam.getGas(steamToAdd), true);
+                                    if (inserted >= TesseractGraphWrappers.dropletMultiplier){
+                                        heatMultiplier = Math.min(heatMultiplier, (int)((inserted / TesseractGraphWrappers.dropletMultiplier) / tEfficiency));
+                                        f.drainInput(DistilledWater.getLiquid(heatMultiplier), false);
+                                        f.getOutputTanks().getTank(1).internalInsert(steam.getGas(steamToAdd), false);
+                                        h.extract(heatMultiplier * 80, false);
+                                    }
+                                }
+                                dryHeatCounter = 0;
+                            } else {
+                                dryHeatCounter++;
+                            }
+                        } else {
+                            dryHeatCounter++;
+                        }
+                    }
+                });
+
+            });
+            if (dryHeatCounter >= dryHeatMaximum){
+                explodeMultiblock();
+            }
+        }
+
+    }
+
+    @Override
+    public boolean test(SlotType<?> slotType, int slot, ItemStack stack) {
+        if (slotType == SlotType.STORAGE) return stack.getItem() instanceof ItemIntCircuit;
+        return true;
+    }
+
+    @Override
+    public void onMachineEvent(IMachineEvent event, Object... data) {
+        if (event == SlotType.STORAGE){
+            ItemStack circuit = itemHandler.map(i -> i.getHandler(SlotType.STORAGE).getItem(0)).orElse(ItemStack.EMPTY);
+            if (circuit.getItem() instanceof ItemIntCircuit circuit1){
+                superheatedThreshold = 80000 - (circuit1.circuitId * 3000);
+                efficiency = 1000 - (circuit1.circuitId * 15);
+            } else {
+                superheatedThreshold = 80000;
+                efficiency = 1000;
+            }
+        }
+        super.onMachineEvent(event, data);
+    }
+
+    @Override
+    public void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.putInt("efficiency", efficiency);
+        tag.putInt("superheatedThreshold", superheatedThreshold);
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        efficiency = tag.getInt("efficiency");
+        superheatedThreshold = tag.getInt("superheatedThreshold");
+    }
 
     @Override
     public int drawInfo(InfoRenderWidget.MultiRenderWidget instance, PoseStack stack, Font renderer, int left, int top) {
@@ -35,72 +190,6 @@ public class BlockEntityHeatExchanger extends BlockEntityMultiMachine<BlockEntit
     @Override
     public WidgetSupplier getInfoWidget() {
         return HeatInfoWidget.build().setPos(10, 10);
-    }
-
-    public BlockEntityHeatExchanger(Machine type, BlockPos pos, BlockState state) {
-        super(type, pos, state);
-        heatHandler.set(() -> new DefaultHeatHandler(this, 80 * 4, 80, 0));
-        recipeHandler.set(() -> new MachineRecipeHandler<>(this){
-            @Override
-            protected boolean canRecipeContinue() {
-                return super.canRecipeContinue() && heatHandler.map(h -> h.getHeat() + (activeRecipe.getPower() * activeRecipe.getDuration()) <= h.getHeatCap()).orElse(false);
-            }
-
-            @Override
-            public boolean canOutput() {
-                //ignore chance for canOutput.
-                if (tile.itemHandler.isPresent() && activeRecipe.hasOutputItems() && !tile.itemHandler.map(t -> t.canOutputsFit(activeRecipe.getFlatOutputItems())).orElse(false))
-                    return false;
-                return !tile.fluidHandler.isPresent() || !activeRecipe.hasOutputFluids() || tile.fluidHandler.map(t -> t.getOutputTanks() != null && t.getOutputTanks().getSize() > 1 && t.getOutputTanks().getTank(0).internalInsert(activeRecipe.getOutputFluids()[0], true) == activeRecipe.getOutputFluids()[0].getFluidAmount()).orElse(false);
-            }
-
-            @Override
-            public boolean consumeResourceForRecipe(boolean simulate) {
-                return heatHandler.map(h -> h.insert((int) activeRecipe.getPower(), simulate) > 0).orElse(false);
-            }
-
-            protected void addOutputs() {
-                if (activeRecipe.hasOutputItems()) {
-                    tile.itemHandler.ifPresent(h -> {
-                        //Roll the chances here. If they don't fit add flat (no chances).
-                        ItemStack[] out = activeRecipe.getOutputItems(true);
-                        if (h.canOutputsFit(out)) {
-                            h.addOutputs(out);
-                        } else {
-                            h.addOutputs(activeRecipe.getFlatOutputItems());
-                        }
-                        tile.onMachineEvent(MachineEvent.ITEMS_OUTPUTTED);
-                    });
-                }
-                if (activeRecipe.hasOutputFluids()) {
-                    tile.fluidHandler.ifPresent(h -> {
-                        if (h.getOutputTanks() == null) return;
-                        h.getOutputTanks().getTank(0).internalInsert(activeRecipe.getOutputFluids()[0], false);
-                        tile.onMachineEvent(MachineEvent.FLUIDS_OUTPUTTED);
-                    });
-                }
-            }
-        });
-
-    }
-
-    @Override
-    public void serverTick(Level level, BlockPos pos, BlockState state) {
-        super.serverTick(level, pos, state);
-        fluidHandler.ifPresent(f -> {
-            heatHandler.ifPresent(h -> {
-                if (h.getHeat() >= 80){
-                    if (f.drainInput(Water.getLiquid(1), true).getFluidAmount() == TesseractGraphWrappers.dropletMultiplier){
-                        if (f.getOutputTanks() != null && f.getOutputTanks().getSize() >= 2 && f.getOutputTanks().getTank(1).internalInsert(Steam.getGas(160), true) == 160 * TesseractGraphWrappers.dropletMultiplier){
-                            f.drainInput(Water.getLiquid(1), false);
-                            f.getOutputTanks().getTank(1).internalInsert(Steam.getGas(160), false);
-                            h.extract(80, false);
-                        }
-                    }
-                }
-            });
-
-        });
     }
 
     public static class HeatInfoWidget extends InfoRenderWidget.MultiRenderWidget {
--- common/src/main/java/muramasa/gregtech/loader/multi/HeatExchangerLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/multi/HeatExchangerLoader.java
@@ -12,17 +12,17 @@ public class HeatExchangerLoader {
     public static void init() {
         HEAT_EXCHANGER.RB().fi(HotCoolant.getLiquid(1))
                 .fo(Coolant.getLiquid(1))
-                .add("hot_coolant",20, 1);
+                .add("hot_coolant",1, 20);
         HEAT_EXCHANGER.RB().fi(Lava.getLiquid(1))
                 .fo(FluidHooks.newFluidHolder(GTCoreFluids.PAHOEHOE_LAVA.getFluid(), TesseractGraphWrappers.dropletMultiplier, null))
-                .add("lava", 5, 16);
-        HEAT_EXCHANGER.RB().fi(HotCarbonDioxide.getGas(1)).fo(CarbonDioxide.getGas(1)).add("hot_carbon_dioxide", 20, 1);
-        HEAT_EXCHANGER.RB().fi(HotMoltenSodium.getLiquid(1)).fo(Sodium.getLiquid(1)).add("hot_molten_sodium", 30, 1);
-        HEAT_EXCHANGER.RB().fi(HotMoltenTin.getLiquid(1)).fo(Tin.getLiquid(1)).add("hot_molten_tin", 40, 1);
-        HEAT_EXCHANGER.RB().fi(HotMoltenLithiumChloride.getLiquid(1)).fo(LithiumChloride.getLiquid(1)).add("hot_molten_lithium_chloride", 15, 1);
-        HEAT_EXCHANGER.RB().fi(HotHeavyWater.getLiquid(1)).fo(HeavyWater.getLiquid(1)).add("hot_heavy_water", 50, 1);
-        HEAT_EXCHANGER.RB().fi(HotSemiheavyWater.getLiquid(1)).fo(SemiheavyWater.getLiquid(1)).add("hot_semiheavy_water", 40, 1);
-        HEAT_EXCHANGER.RB().fi(HotTritiatedWater.getLiquid(1)).fo(TritiatedWater.getLiquid(1)).add("hot_tritiated_water", 60, 1);
+                .add("lava", 1, 80);
+        HEAT_EXCHANGER.RB().fi(HotCarbonDioxide.getGas(1)).fo(CarbonDioxide.getGas(1)).add("hot_carbon_dioxide", 1, 20);
+        HEAT_EXCHANGER.RB().fi(HotMoltenSodium.getLiquid(1)).fo(Sodium.getLiquid(1)).add("hot_molten_sodium", 1, 30);
+        HEAT_EXCHANGER.RB().fi(HotMoltenTin.getLiquid(1)).fo(Tin.getLiquid(1)).add("hot_molten_tin", 1, 40);
+        HEAT_EXCHANGER.RB().fi(HotMoltenLithiumChloride.getLiquid(1)).fo(LithiumChloride.getLiquid(1)).add("hot_molten_lithium_chloride", 1, 15);
+        HEAT_EXCHANGER.RB().fi(HotHeavyWater.getLiquid(1)).fo(HeavyWater.getLiquid(1)).add("hot_heavy_water", 1, 50);
+        HEAT_EXCHANGER.RB().fi(HotSemiheavyWater.getLiquid(1)).fo(SemiheavyWater.getLiquid(1)).add("hot_semiheavy_water", 1, 40);
+        HEAT_EXCHANGER.RB().fi(HotTritiatedWater.getLiquid(1)).fo(TritiatedWater.getLiquid(1)).add("hot_tritiated_water", 1, 60);
         HEAT_EXCHANGER.RB().fi(HotHelium.getGas(1)).fo(Helium.getGas(1)).add("hot_helium", 30, 1);
     }
 }
