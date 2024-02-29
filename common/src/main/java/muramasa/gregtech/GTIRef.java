a1b3b473a22ef57fe835d2f388e84a2b335f70cd
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-02-29 15:02:33 -0500

    changed mod name from GregTech to GTI

--- common/src/main/java/muramasa/gregtech/GTIRef.java
+++ common/src/main/java/muramasa/gregtech/GTIRef.java
@@ -7,7 +7,7 @@ public class GTIRef {
 
     /** Mod Data **/
     public static final String ID = "gti";
-    public static final String NAME = "GregTech";
+    public static final String NAME = "GTI";
     public static final String VERSION = "0.0.1";
     public static final String ANTIMATTER = Ref.ID;
     public static final String ANTIMATTER_SHARED = Ref.SHARED_ID;
