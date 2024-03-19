413d334fdcb9675b100875c242963497b87d16a7
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-03-19 17:03:04 -0400

    fixed config comments

--- common/src/main/java/muramasa/gregtech/GregTechConfig.java
+++ common/src/main/java/muramasa/gregtech/GregTechConfig.java
@@ -25,11 +25,11 @@ public class GregTechConfig {
         ConfigSection section = config.add("general");
         /*MORE_COMPLICATED_CHEMICAL_RECIPES = section.addBool("more_complicated_chemical_recipes", false, "Enables more complicated chemical recipes. - Default: false");
         HARDER_CIRCUITS = section.addBool("harder_circuits", false, "Enables more complicated circuit recipes added in versions of gt5u after 509.25 - Default: false");*/
-        GT5U_OIL = section.addBool("gt5u_oil", false, "Enables gt5u oil processing, if false 6 oil processing is used instead. - Default: false");
+        GT5U_OIL = section.addBool("gt5u_oil", false, "Enables gt5u oil processing, if false gt6 oil processing is used instead. - Default: false");
         HARDER_ALUMINIUM_PROCESSING = section.addBool("harder_aluminium_processing", true, "Enables gt6's alumina processing, if disabled alumina reverts back to just being in the blast furnace - Default: true");
         GT6_ORE_GEN = section.addBool("gt6_ore_gen", false, "Enables gt6 style veins insteadof gt5 style veins. - Default: false");
         ASPHALT_MULTIPLIER = section.addDouble("asphalt_multiplier", 1.1, "Default speed multiplier applied by concrete.");
-        HARD_CARBON = section.addBool("hard_carbon", false, "Makes carbon fibre require the hard recipe from 5u. - Default: false");
+        HARD_CARBON = section.addBool("hard_carbon", false, "Makes carbon fibre require the hard recipe from gt5u. - Default: false");
         COMPLICATED_CHEMICAL_PROCESSING = section.addBool("complicated_chemical_processing", false, "Enables complicated chemical recipes");
         CONFIG = AntimatterPlatformUtils.createConfig(GTIRef.ID, config);
         CONFIG.register();
