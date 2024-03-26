1b37c88608c5997b1bd5d194e12d3ea7eb2fbbc0
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-03-26 15:49:10 -0400

    fixed dense plate recipes in bender

--- common/src/main/java/muramasa/gregtech/loader/machines/BenderLoader.java
+++ common/src/main/java/muramasa/gregtech/loader/machines/BenderLoader.java
@@ -26,10 +26,10 @@ public class BenderLoader {
         PLATE_DENSE.all().forEach(t -> {
             if (t.has(MaterialTags.RUBBERTOOLS)) return;
             if (t.has(AntimatterMaterialTypes.INGOT)) {
-                BENDER.RB().ii(AntimatterMaterialTypes.INGOT.getMaterialIngredient(t,1),INT_CIRCUITS.get(9)).io(PLATE_DENSE.get(t,1)).add("plate_dense_" + t.getId(), baseDuration.applyAsLong(t) * 9, 96);
+                BENDER.RB().ii(AntimatterMaterialTypes.INGOT.getMaterialIngredient(t,9),INT_CIRCUITS.get(9)).io(PLATE_DENSE.get(t,1)).add("plate_dense_" + t.getId(), baseDuration.applyAsLong(t) * 9, 96);
             }
             if (t.has(PLATE)){
-                BENDER.RB().ii(PLATE.getMaterialIngredient(t,1),INT_CIRCUITS.get(9)).io(PLATE_DENSE.get(t,1)).add("plate_dense_" + t.getId() + "_from_plate", baseDuration.applyAsLong(t) * 9, 96);
+                BENDER.RB().ii(PLATE.getMaterialIngredient(t,9),INT_CIRCUITS.get(9)).io(PLATE_DENSE.get(t,1)).add("plate_dense_" + t.getId() + "_from_plate", baseDuration.applyAsLong(t) * 9, 96);
             }
         });
         AntimatterMaterialTypes.FOIL.all().forEach(foil -> {
